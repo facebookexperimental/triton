d784ee5acbad3ff59a7733ce09691a3caa883aad
dshi7 <shidaohang1990@gmail.com>
2025-05-21 09:31:18 -0700

    pytest python/test/unit/language/test_tlx.py -k test_mbarriers

--- third_party/tlx/language/__init__.py
+++ third_party/tlx/language/__init__.py
@@ -1,18 +1,10 @@
 from .async_task import async_task, async_tasks
-from .types import * 
+from .types import *
 from .mem_ops import *
 from .barrier import *
 from .utility import *
 
 __all__ = [
-    "alloc_barriers",
-    "async_task",
-    "async_tasks",
-    "buffered_tensor",
-    "mbarriers",
-    "local_alloc",
-    "local_view",
-    "alloc_barriers",
-    "barrier_expect",
-    "thread_id"
+    "alloc_barriers", "async_task", "async_tasks", "buffered_tensor", "mbarriers", "local_alloc", "local_view",
+    "barrier_expect", "thread_id"
 ]
