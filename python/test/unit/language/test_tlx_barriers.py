501825e91f58aea1f3f0a63cc864adcf5816e09a
dshi7 <shidaohang1990@gmail.com>
2025-05-23 09:36:08 -0700

    improve unit test

--- python/test/unit/language/test_tlx_barriers.py
+++ python/test/unit/language/test_tlx_barriers.py
@@ -6,7 +6,6 @@ from triton._internal_testing import is_cuda
 import triton.tlx.language as tlx
 
 
-# Define triton kernels for unit tests
 @triton.jit
 def tlx_square_non_ws(
     x_ptr,
@@ -14,6 +13,26 @@ def tlx_square_non_ws(
     n_elements,
     BLOCK_SIZE: tl.constexpr,
 ):
+    """
+    Test pairs of arrive/wait using different phases
+    with a few random misc operations interleaved between them.
+
+    To learn more about mbarrier phase, refer to:
+    https://docs.nvidia.com/cuda/parallel-thread-execution/#data-movement-and-conversion-instructions-asynchronous-copy-completion-mechanisms-mbarrier
+
+    Following patterns will cause mbarrier deadlock.
+    TODO. add unit tests demonstrating mbarrier deadlock
+
+    Case 1:
+    arrive => wait(phase=1)
+
+    Case 2:
+    arrive => arrive => wait(phase=0)
+
+    Case 3:
+    wait(phase=0) => arrive
+    """
+
     # prologue
     pid = tl.program_id(axis=0)
     block_start = pid * BLOCK_SIZE
@@ -21,15 +40,25 @@ def tlx_square_non_ws(
     mask = offsets < n_elements
 
     # mbarrier ops
+
     bars = tlx.alloc_barriers(num_barriers=1)  # create
     bar = tlx.local_view(bars, 0)
+
+    x = tl.load(x_ptr + offsets, mask=mask)  # Do something
+
     tlx.barrier_arrive(bar=bar)  # Release
     tlx.barrier_wait(bar=bar, phase=0)  # Wait (proceed immediately)
 
-    # Some arith ops TODO. add WS
-    x = tl.load(x_ptr + offsets, mask=mask)
-    z = x * x
-    tl.store(z_ptr + offsets, z, mask=mask)
+    z = x * x  # Do something
+
+    tlx.barrier_arrive(bar=bar)  # Release
+    tlx.barrier_wait(bar=bar, phase=1)  # Wait (proceed immediately)
+
+    tl.store(z_ptr + offsets, z, mask=mask)  # Do something
+
+    tlx.barrier_arrive(bar=bar)  # Release
+    tlx.barrier_wait(bar=bar, phase=0)  # Wait (proceed immediately)
+
 
 @triton.jit
 def tlx_square_ws(
@@ -43,15 +72,22 @@ def tlx_square_ws(
     block_start = pid * BLOCK_SIZE
 
     # mbarrier ops
-    bars = tlx.alloc_barriers(num_barriers=1)  # create
-    bar = tlx.local_view(bars, 0)
+    bars = tlx.alloc_barriers(num_barriers=2)  # create
+    b0 = tlx.local_view(bars, 0)
+    b1 = tlx.local_view(bars, 1)
+
+    tlx.barrier_arrive(bar=b1)  # Kick off the 1st release to proceed the wait
 
     with tlx.async_tasks():
         with tlx.async_task("default"):
-            tlx.barrier_arrive(bar=bar)  # Release
+            tlx.barrier_wait(bar=b1, phase=0)
+
+            # Placeholder block to do something
+
+            tlx.barrier_arrive(bar=b0)  # Release
 
         with tlx.async_task(num_warps=4):
-            tlx.barrier_wait(bar=bar, phase=0)  # Wait (proceed immediately)
+            tlx.barrier_wait(bar=b0, phase=0)  # Wait (proceed immediately)
 
             # Some arith ops TODO. add WS
             offsets = block_start + tl.arange(0, BLOCK_SIZE)
@@ -61,14 +97,40 @@ def tlx_square_ws(
             tl.store(z_ptr + offsets, z, mask=mask)
 
 
-# Unit test for arrive/wait
-@pytest.mark.skipif(
-    not is_cuda() or torch.cuda.get_device_capability()[0] < 9,
-    reason="Requires compute capability >= 9 for NV",
-)
-@pytest.mark.parametrize("func, BLOCK_SIZE", [(f, 1024) for f in [tlx_square_non_ws, tlx_square_ws]])
-# def test_mbarriers(BLOCK_SIZE, device):
-def test_wait_arrive(func, BLOCK_SIZE, device):
+@triton.jit
+def tlx_square_ws_phase(
+    x_ptr,
+    z_ptr,
+    n_elements,
+    BLOCK_SIZE: tl.constexpr,
+):
+    # prologue
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+
+    # mbarrier ops
+    bars = tlx.alloc_barriers(num_barriers=2)  # create
+    b0 = tlx.local_view(bars, 0)
+    b1 = tlx.local_view(bars, 0)
+
+    with tlx.async_tasks():
+        with tlx.async_task("default"):
+            tlx.barrier_arrive(bar=b0)  # Release b0
+
+        with tlx.async_task(num_warps=4):
+            tlx.barrier_wait(bar=b0, phase=0)  # Wait b0
+
+            # Some arith ops TODO. add WS
+            offsets = block_start + tl.arange(0, BLOCK_SIZE)
+            mask = offsets < n_elements
+            x = tl.load(x_ptr + offsets, mask=mask)
+            z = x * x
+            tl.store(z_ptr + offsets, z, mask=mask)
+
+            tlx.barrier_arrive(bar=b1, phase=0)  # Wait b0
+
+
+def run_tlx_square(func, BLOCK_SIZE, device):
 
     # prepare inputs
     torch.manual_seed(0)
@@ -83,12 +145,40 @@ def test_wait_arrive(func, BLOCK_SIZE, device):
 
     kernel = func[grid](x, z, n_elements, BLOCK_SIZE)
 
-    # ASSERT in ttgir
-    ttgir = kernel.asm["ttgir"]
-    assert (ttgir.count("ttng.init_barrier") == 1) and (ttgir.count("ttng.wait_barrier") == 1) and (
-        ttgir.count("ttng.barrier_expect") == 0) and (ttgir.count("ttng.arrive_barrier") == 1), f"TTGIR {ttgir}"
-
     z_ref = x * x
 
     torch.testing.assert_close(z, z_ref, check_dtype=False)
+    return kernel
 
+
+# Unit test for arrive/wait
+@pytest.mark.skipif(
+    not is_cuda() or torch.cuda.get_device_capability()[0] < 9,
+    reason="Requires compute capability >= 9 for NV",
+)
+@pytest.mark.parametrize("BLOCK_SIZE", [(1024)])
+# def test_mbarriers(BLOCK_SIZE, device):
+def test_wait_arrive_no_ws(BLOCK_SIZE, device):
+    kernel = run_tlx_square(tlx_square_non_ws, BLOCK_SIZE, device)
+
+    # ASSERT in ttgir
+    ttgir = kernel.asm["ttgir"]
+    assert (ttgir.count("ttng.init_barrier") == 1) and (ttgir.count("ttng.wait_barrier") == 3) and (
+        ttgir.count("ttng.barrier_expect") == 0) and (ttgir.count("ttng.arrive_barrier") == 3), f"TTGIR {ttgir}"
+
+
+@pytest.mark.skipif(
+    not is_cuda() or torch.cuda.get_device_capability()[0] < 9,
+    reason="Requires compute capability >= 9 for NV",
+)
+@pytest.mark.parametrize("BLOCK_SIZE", [(1024)])
+# def test_mbarriers(BLOCK_SIZE, device):
+def test_wait_arrive_ws(BLOCK_SIZE, device):
+    kernel = run_tlx_square(tlx_square_ws, BLOCK_SIZE, device)
+
+    # ASSERT in ttgir
+    ttgir = kernel.asm["ttgir"]
+    assert (ttgir.count("ttng.init_barrier")
+            == 2) and (ttgir.count("ttng.wait_barrier") == 2) and (ttgir.count("ttng.barrier_expect") == 0) and (
+                ttgir.count("ttng.arrive_barrier")
+                == 2) and (ttgir.count("default {") == 1) and (ttgir.count("partition0") == 1), f"TTGIR {ttgir}"
