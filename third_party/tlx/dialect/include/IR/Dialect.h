eb8fc702c8e676aca8dfd8d7a943eb3b466c4c40
Hongtao Yu <hoy@fb.com>
2025-05-29 17:18:08 -0700

    link tlx

--- third_party/tlx/dialect/include/IR/Dialect.h
+++ third_party/tlx/dialect/include/IR/Dialect.h
@@ -1,5 +1,5 @@
-#ifndef DIALECT_TLX_IR_DIALECT_H_
-#define DIALECT_TLX_IR_DIALECT_H_
+#ifndef TRITON_DIALECT_TLX_IR_DIALECT_H_
+#define TRITON_DIALECT_TLX_IR_DIALECT_H_
 
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/IR/BuiltinOps.h"
@@ -7,10 +7,13 @@
 #include "mlir/IR/PatternMatch.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
 
-#include "IR/Dialect.h.inc"
+#include "tlx/dialect/include/IR/Dialect.h.inc"
+#include "tlx/dialect/include/IR/OpsEnums.h.inc"
+#define GET_ATTRDEF_CLASSES
+#include "tlx/dialect/include/IR/TLXAttrDefs.h.inc"
 
 #define GET_OP_CLASSES
-#include "IR/Ops.h.inc"
+#include "tlx/dialect/include/IR/Ops.h.inc"
 
 namespace mlir {
 namespace triton {
@@ -18,4 +21,4 @@ namespace tlx {} // namespace proton
 } // namespace triton
 } // namespace mlir
 
-#endif // DIALECT_TLX_IR_DIALECT_H_
+#endif // TRITON_DIALECT_TLX_IR_DIALECT_H_
