2995e3704e3ca93bce2d33cefe50a846aacffe9d
dshi7 <shidaohang1990@gmail.com>
2025-06-06 13:45:53 -0700

    reuse semantic dot in tlx async_dot

--- third_party/tlx/dialect/triton_tlx.cc
+++ third_party/tlx/dialect/triton_tlx.cc
@@ -78,6 +78,41 @@ void init_triton_tlx_ir(py::module &&m) {
               unsigned pendings) -> mlir::Value {
              return self.create<ttg::AsyncWaitOp>(asyncTokens, pendings);
            });
+      .def(
+          "create_require_layout",
+          [](TritonOpBuilder &self, Value &v, int opIdx) -> Value {
+
+            Value arg = v;
+
+            auto argType = cast<RankedTensorType>(arg.getType());
+            assert(argType.getEncoding() && "unexpected tensor type");
+
+            auto order = ttg::getOrderForMemory(argType);
+            llvm::SmallVector<unsigned> newOrder = order;
+            if (opIdx == 1) {
+              newOrder = {0, 1};
+            } else {
+              newOrder = {1, 0};
+            }
+
+            Attribute SharedMemorySpace =
+                ttg::SharedMemorySpaceAttr::get(argType.getContext());
+            auto CTALayout = ttg::getCTALayout(argType.getEncoding());
+            auto newLayout = ttg::NVMMASharedEncodingAttr::get(
+                argType.getContext(), argType.getShape(), newOrder, CTALayout,
+                argType.getElementType(), false);
+            auto newType = ttg::MemDescType::get(argType.getShape(), argType.getElementType(),
+                                            newLayout, SharedMemorySpace);
+
+            // if (auto type = dyn_cast<ttg::MemDescType>(v.getType())) {
+            //   newType = ttg::MemDescType::get(
+            //       type.getShape(), type.getElementType(), encoding,
+            //       type.getMemorySpace(), type.getMutableMemory());
+            // } else {
+            //   throw std::runtime_error("Unsupported type");
+            // }
+            return self.create<tlx::RequireLayoutOp>(newType, v);
+          });
 }
 
 void init_triton_tlx_passes(py::module &&m) {
