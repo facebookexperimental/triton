aa5687f159743c8396664cac50543765749215ff
dshi7 <shidaohang1990@gmail.com>
2025-06-09 11:15:19 -0700

    swizzled_shared => nvmma_shared

--- third_party/tlx/dialect/triton_tlx.cc
+++ third_party/tlx/dialect/triton_tlx.cc
@@ -15,8 +15,8 @@ void init_triton_tlx_ir(py::module &&m) {
       [](TritonOpBuilder &self, Value &v, Attribute &encoding) -> Value {
         if (auto type = dyn_cast<ttg::MemDescType>(v.getType())) {
           auto newType = ttg::MemDescType::get(
-              type.getShape(), type.getElementType(),
-              dyn_cast<ttg::NvidiaMmaEncodingAttr>(encoding),
+              type.getShape(), type.getElementType(), encoding,
+              // dyn_cast<ttg::NvidiaMmaEncodingAttr>(encoding),
               type.getMemorySpace(), type.getMutableMemory());
           return self.create<tlx::RequireLayoutOp>(newType, v);
         } else {
