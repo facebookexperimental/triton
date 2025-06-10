66d0e9cb61014312276eba0254f94bb9cbb3a23d
dshi7 <shidaohang1990@gmail.com>
2025-06-09 18:31:47 -0700

    rebase patch fix 3 - remove softlink third_party/tlx

--- third_party/tlx/compiler/dispatch.py
+++
@@ -1,9 +0,0 @@
-import ast
-import triton.tlx.language as tlx
-from triton.tlx.compiler import visit_withAsyncTask, visit_withAsyncTasks
-
-# Dispatch table
-TLX_WITH_DISPATCH = {
-    tlx.async_tasks: visit_withAsyncTasks,
-    tlx.async_task: visit_withAsyncTask,
-}
