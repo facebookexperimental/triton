276702a75b6b727ff4782e578556a728136d4463
dshi7 <shidaohang1990@gmail.com>
2025-06-09 17:28:39 -0700

    rebase and fix => now it works

--- third_party/tlx/dialect/triton_tlx.cc
+++ third_party/tlx/dialect/triton_tlx.cc
@@ -13,19 +13,7 @@ namespace ttng = triton::nvidia_gpu;
 void init_triton_tlx_ir(py::module &&m) {
   auto *builder_cls = ir::getBuilderClass();
   builder_cls
-      ->def("create_require_layout",
-            [](TritonOpBuilder &self, Value &v, Attribute &encoding) -> Value {
-              Type newType;
-              if (auto type = dyn_cast<ttg::MemDescType>(v.getType())) {
-                newType = ttg::MemDescType::get(
-                    type.getShape(), type.getElementType(), encoding,
-                    type.getMemorySpace(), type.getMutableMemory());
-              } else {
-                throw std::runtime_error("Unsupported type");
-              }
-              return self.create<tlx::RequireLayoutOp>(newType, v);
-            })
-      .def("make_tensor_memory_encoding_attr",
+      ->def("make_tensor_memory_encoding_attr",
            [](TritonOpBuilder &self, unsigned blockM, unsigned blockN,
               bool unpacked, unsigned CTASplitM, unsigned CTASplitN) {
              auto context = self.getBuilder().getContext();
@@ -52,65 +40,31 @@ void init_triton_tlx_ir(py::module &&m) {
               unsigned pendings) -> mlir::Value {
              return self.create<ttg::AsyncWaitOp>(asyncTokens, pendings);
            })
-      .def(
-      "create_require_layout",
-      [](TritonOpBuilder &self, Value &v, Attribute &encoding) -> Value {
-        if (auto type = dyn_cast<ttg::MemDescType>(v.getType())) {
-          auto newType = ttg::MemDescType::get(
-              type.getShape(), type.getElementType(),
-              dyn_cast<ttg::NvidiaMmaEncodingAttr>(encoding),
-              type.getMemorySpace(), type.getMutableMemory());
-          return self.create<tlx::RequireLayoutOp>(newType, v);
-        } else {
-          throw std::runtime_error("Unsupported type");
-        }
-      }
-      .def(
-      "create_convert_layout",
-      [](TritonOpBuilder &self, Value &v, Attribute &encoding) -> Value {
-        Type newType;
-        if (auto type = dyn_cast<ttg::MemDescType>(v.getType())) {
-          newType = ttg::MemDescType::get(
-              type.getShape(), type.getElementType(), encoding,
-              type.getMemorySpace(), type.getMutableMemory());
-          return self.create<tlx::RequireLayoutOp>(newType, v);
-        } else if (auto type = dyn_cast<RankedTensorType>(v.getType())) {
-          newType = RankedTensorType::get(type.getShape(),
-                                          type.getElementType(), encoding);
-          return self.create<ttg::ConvertLayoutOp>(newType, v);
-        } else {
-          throw std::runtime_error("Unsupported type");
-        }
-        newType.dump();
-      })
-      ->def("create_require_layout",
-            [](TritonOpBuilder &self, Value &v, Attribute &encoding) -> Value {
-              Type newType;
-              if (auto type = dyn_cast<ttg::MemDescType>(v.getType())) {
-                newType = ttg::MemDescType::get(
-                    type.getShape(), type.getElementType(), encoding,
-                    type.getMemorySpace(), type.getMutableMemory());
-              } else {
-                throw std::runtime_error("Unsupported type");
-              }
+      .def("create_convert_layout",
+          [](TritonOpBuilder &self, Value &v, Attribute &encoding) -> Value {
+            Type newType;
+            if (auto type = dyn_cast<ttg::MemDescType>(v.getType())) {
+              newType = ttg::MemDescType::get(
+                  type.getShape(), type.getElementType(), encoding,
+                  type.getMemorySpace(), type.getMutableMemory());
               return self.create<tlx::RequireLayoutOp>(newType, v);
-            })
-      .def("make_tensor_memory_encoding_attr",
-           [](TritonOpBuilder &self, unsigned blockM, unsigned blockN,
-              bool unpacked, unsigned CTASplitM, unsigned CTASplitN) {
-             auto context = self.getBuilder().getContext();
-             return mlir::cast<Attribute>(ttng::TensorMemoryEncodingAttr::get(
-                 context, blockM, blockN, unpacked, CTASplitM, CTASplitN));
-           })
-      .def("create_tmem_alloc",
-           [](TritonOpBuilder &self, std::vector<int64_t> shape,
-              Type &elementType, Attribute &encoding) -> mlir::Value {
-             auto context = self.getBuilder().getContext();
-             auto memorySpace = ttng::TensorMemorySpaceAttr::get(context);
-             auto memDesc =
-                 ttg::MemDescType::get(shape, elementType, encoding,
-                                       memorySpace, /*mutableMemory=*/true);
-             return self.create<ttng::TMEMAllocOp>(memDesc, nullptr);
+            } else if (auto type = dyn_cast<RankedTensorType>(v.getType())) {
+              newType = RankedTensorType::get(type.getShape(),
+                                              type.getElementType(), encoding);
+              return self.create<ttg::ConvertLayoutOp>(newType, v);
+            } else {
+              throw std::runtime_error("Unsupported type");
+            }
+            newType.dump();
+          })
+      .def("create_local_load",
+           [](TritonOpBuilder &self, Value subView,
+              std::optional<Value> asyncToken) -> mlir::Value {
+             auto subViewType = cast<ttg::MemDescType>(subView.getType());
+             auto newType = RankedTensorType::get(subViewType.getShape(),
+                                                  subViewType.getElementType());
+             return self.create<ttg::LocalLoadOp>(newType, subView,
+                                                  asyncToken.value_or(Value()));
            })
       .def(
           "create_require_layout",
