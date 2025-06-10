6faa79e08e52ce20f33a776e1645a91fd9aa7c6d
dshi7 <shidaohang1990@gmail.com>
2025-06-10 13:58:04 -0700

    merge and linter unit test

--- python/test/unit/language/test_tlx.py
+++ python/test/unit/language/test_tlx.py
@@ -55,7 +55,7 @@ def test_async_tasks(BLOCK_SIZE, device):
     output1 = torch.empty_like(x)
     output2 = torch.empty_like(a)
     n_elements = output1.numel()
-    grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
+    grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]), )
     kernel = add2_warp_specialized_kernel[grid](x, y, output1, a, b, output2, n_elements, BLOCK_SIZE)
     ttgir = kernel.asm["ttgir"]
     assert "ttg.warp_specialize" in ttgir
@@ -87,7 +87,7 @@ def test_local_load(BLOCK_SIZE, device):
         x_ptr_offsets = x_ptr + offsets
         y_ptr_offsets = y_ptr + offsets
 
-        buffers = tlx.local_alloc((BLOCK_SIZE,), tl.float32, tl.constexpr(2))
+        buffers = tlx.local_alloc((BLOCK_SIZE, ), tl.float32, tl.constexpr(2))
         buffer0 = tlx.local_view(buffers, 0)
         buffer1 = tlx.local_view(buffers, 1)
         tlx.async_load(x_ptr_offsets, buffer0, mask=mask)
@@ -124,9 +124,7 @@ def test_local_load(BLOCK_SIZE, device):
 def test_tmem_alloc_index(BLOCK_SIZE, device):
 
     @triton.jit
-    def kernel(
-        BLOCK_SIZE: tl.constexpr,
-    ):
+    def kernel(BLOCK_SIZE: tl.constexpr, ):
         buffers = tlx.local_alloc((BLOCK_SIZE, BLOCK_SIZE), tl.float32, tl.constexpr(2), tlx.storage_kind.tmem)
         buffer0 = tlx.local_view(buffers, 0)
         buffer1 = tlx.local_view(buffers, 1)
@@ -141,9 +139,12 @@ def test_tmem_alloc_index(BLOCK_SIZE, device):
 def test_thread_id(device):
 
     @triton.jit
-    def store_from_thread_0_kernel(output_ptr, value, n_elements, axis : tl.constexpr,
+    def store_from_thread_0_kernel(
+        output_ptr,
+        value,
+        n_elements,
+        axis: tl.constexpr,
         BLOCK_SIZE: tl.constexpr,
-
     ):
         pid = tl.program_id(axis=0)
         block_start = pid * BLOCK_SIZE
@@ -156,7 +157,7 @@ def test_thread_id(device):
     output = torch.zeros(32, dtype=torch.int32, device='cuda')
     n_elements = output.numel()
     value = 42
-    store_from_thread_0_kernel[(1,)](output, value, n_elements, 0, 32, num_warps=1)
+    store_from_thread_0_kernel[(1, )](output, value, n_elements, 0, 32, num_warps=1)
     torch.cuda.synchronize()
     expected_output = torch.zeros(32, dtype=torch.int32, device='cuda')
     expected_output[0] = value
@@ -182,7 +183,7 @@ def test_async_wait(BLOCK_SIZE, device):
         offsets = block_start + tl.arange(0, BLOCK_SIZE)
         mask = offsets < n_elements
         input_ptr_offsets = input_ptr + offsets
-        buffers = tlx.local_alloc((BLOCK_SIZE,), tl.float32, tl.constexpr(1))
+        buffers = tlx.local_alloc((BLOCK_SIZE, ), tl.float32, tl.constexpr(1))
         buffer = tlx.local_view(buffers, 0)
         tlx.async_load(input_ptr_offsets, buffer, mask=mask)
         tlx.async_load_commit_group()
@@ -202,7 +203,7 @@ def test_async_wait(BLOCK_SIZE, device):
         offsets = block_start + tl.arange(0, BLOCK_SIZE)
         mask = offsets < n_elements
         input_ptr_offsets = input_ptr + offsets
-        buffers = tlx.local_alloc((BLOCK_SIZE,), tl.float32, tl.constexpr(1))
+        buffers = tlx.local_alloc((BLOCK_SIZE, ), tl.float32, tl.constexpr(1))
         buffer = tlx.local_view(buffers, 0)
         token = tlx.async_load(input_ptr_offsets, buffer, mask=mask)
         token = tlx.async_load_commit_group([token])
@@ -235,7 +236,8 @@ def test_local_trans(device):
     def local_trans_kernel(
         input_ptr,
         output_ptr,
-        M, N,
+        M,
+        N,
         BLOCK_SIZE_M: tl.constexpr,
         BLOCK_SIZE_N: tl.constexpr,
     ):
@@ -264,14 +266,8 @@ def test_local_trans(device):
     BLOCK_SIZE_M, BLOCK_SIZE_N = 32, 64
     x = torch.rand((M, N), dtype=torch.float32, device=device)
     y = torch.empty((N, M), dtype=torch.float32, device=device)
-    grid = lambda meta: (
-        triton.cdiv(M, BLOCK_SIZE_M),
-        triton.cdiv(N, BLOCK_SIZE_N)
-    )
-    kernel = local_trans_kernel[grid](
-        x, y, M, N,
-        BLOCK_SIZE_M=BLOCK_SIZE_M, BLOCK_SIZE_N=BLOCK_SIZE_N,
-        num_warps=1)
+    grid = lambda meta: (triton.cdiv(M, BLOCK_SIZE_M), triton.cdiv(N, BLOCK_SIZE_N))
+    kernel = local_trans_kernel[grid](x, y, M, N, BLOCK_SIZE_M=BLOCK_SIZE_M, BLOCK_SIZE_N=BLOCK_SIZE_N, num_warps=1)
     assert kernel.asm["ttgir"].count("ttg.memdesc_trans") == 1
     torch.testing.assert_close(y, x.T)
 
@@ -338,3 +334,154 @@ def test_async_dot(device):
     # %51 = "ttg.convert_layout"(%3) : (tensor<64x64xf32>) -> tensor<64x64xf32, #mma>
     # "ttng.fence_async_shared"() <{bCluster = false}> : () -> ()
     # %52 = "ttng.warp_group_dot"(%49, %50, %51) <{inputPrecision = 0 : i32, isAsync = true, maxNumImpreciseAcc = 0 : i32}> : (!ttg.memdesc<64x64xf32, #shared1, #smem, mutable>, !ttg.memdesc<64x64xf32, #shared2, #smem, mutable>, tensor<64x64xf32, #mma>) -> tensor<64x64xf32, #mma>
+
+
+@triton.jit
+def tlx_square_non_ws(
+    x_ptr,
+    z_ptr,
+    n_elements,
+    BLOCK_SIZE: tl.constexpr,
+):
+    """
+    Test pairs of arrive/wait using different phases
+    with a few random misc operations interleaved between them.
+
+    To learn more about mbarrier phase, refer to:
+    https://docs.nvidia.com/cuda/parallel-thread-execution/#data-movement-and-conversion-instructions-asynchronous-copy-completion-mechanisms-mbarrier
+
+    Following patterns will cause mbarrier deadlock.
+    TODO. add unit tests demonstrating mbarrier deadlock
+
+    Case 1:
+    arrive => wait(phase=1)
+
+    Case 2:
+    arrive => arrive => wait(phase=0)
+
+    Case 3:
+    wait(phase=0) => arrive
+    """
+
+    # prologue
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+
+    # mbarrier ops
+
+    bars = tlx.alloc_barriers(num_barriers=1)  # create
+    bar = tlx.local_view(bars, 0)
+
+    x = tl.load(x_ptr + offsets, mask=mask)  # Do something
+
+    p = 0
+    tlx.barrier_arrive(bar=bar)  # Release
+    tlx.barrier_wait(bar=bar, phase=p)  # Wait (proceed immediately)
+
+    z = x * x  # Do something
+
+    p = p ^ 1
+    tlx.barrier_arrive(bar=bar)  # Release
+    tlx.barrier_wait(bar=bar, phase=p)  # Wait (proceed immediately)
+
+    tl.store(z_ptr + offsets, z, mask=mask)  # Do something
+
+    p = p ^ 1
+    tlx.barrier_arrive(bar=bar)  # Release
+    tlx.barrier_wait(bar=bar, phase=0)  # Wait (proceed immediately)
+
+
+@triton.jit
+def tlx_square_ws(
+    x_ptr,
+    z_ptr,
+    n_elements,
+    BLOCK_SIZE: tl.constexpr,
+):
+    # prologue
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+
+    # mbarrier ops
+    bars = tlx.alloc_barriers(num_barriers=2)  # create
+    b0 = tlx.local_view(bars, 0)
+    b1 = tlx.local_view(bars, 1)
+
+    phase = 0
+    with tlx.async_tasks():
+        with tlx.async_task("default"):
+
+            tlx.barrier_wait(bar=b1, phase=phase ^ 1)
+
+            # Placeholder block to do something
+
+            tlx.barrier_arrive(bar=b0)  # Release
+
+        with tlx.async_task(num_warps=4):
+
+            tlx.barrier_wait(bar=b0, phase=phase)  # Wait
+
+            # Some arith ops TODO. add WS
+            offsets = block_start + tl.arange(0, BLOCK_SIZE)
+            mask = offsets < n_elements
+            x = tl.load(x_ptr + offsets, mask=mask)
+            z = x * x
+            tl.store(z_ptr + offsets, z, mask=mask)
+
+            tlx.barrier_arrive(bar=b0)  # Wait
+
+
+def run_tlx_square(func, BLOCK_SIZE, device):
+
+    # prepare inputs
+    torch.manual_seed(0)
+    size = 98432
+    x = torch.rand(size, device=device)
+    z = torch.empty_like(x)
+    z_ref = torch.empty_like(x)
+
+    n_elements = x.numel()
+
+    grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]), )
+
+    kernel = func[grid](x, z, n_elements, BLOCK_SIZE)
+
+    z_ref = x * x
+
+    torch.testing.assert_close(z, z_ref, check_dtype=False)
+    return kernel
+
+
+# Unit test for arrive/wait
+@pytest.mark.skipif(
+    not is_cuda() or torch.cuda.get_device_capability()[0] < 9,
+    reason="Requires compute capability >= 9 for NV",
+)
+@pytest.mark.parametrize("BLOCK_SIZE", [(1024)])
+# def test_mbarriers(BLOCK_SIZE, device):
+def test_wait_arrive_non_ws(BLOCK_SIZE, device):
+    kernel = run_tlx_square(tlx_square_non_ws, BLOCK_SIZE, device)
+
+    # ASSERT in ttgir
+    ttgir = kernel.asm["ttgir"]
+    assert (ttgir.count("ttng.init_barrier") == 1) and (ttgir.count("ttng.wait_barrier") == 3) and (
+        ttgir.count("ttng.barrier_expect") == 0) and (ttgir.count("ttng.arrive_barrier") == 3), f"TTGIR {ttgir}"
+
+
+@pytest.mark.skipif(
+    not is_cuda() or torch.cuda.get_device_capability()[0] < 9,
+    reason="Requires compute capability >= 9 for NV",
+)
+@pytest.mark.parametrize("BLOCK_SIZE", [(1024)])
+# def test_mbarriers(BLOCK_SIZE, device):
+def test_wait_arrive_ws(BLOCK_SIZE, device):
+    kernel = run_tlx_square(tlx_square_ws, BLOCK_SIZE, device)
+
+    # ASSERT in ttgir
+    ttgir = kernel.asm["ttgir"]
+    assert (ttgir.count("ttng.init_barrier")
+            == 2) and (ttgir.count("ttng.wait_barrier") == 2) and (ttgir.count("ttng.barrier_expect") == 0) and (
+                ttgir.count("ttng.arrive_barrier")
+                == 2) and (ttgir.count("default {") == 1) and (ttgir.count("partition0") == 1), f"TTGIR {ttgir}"
