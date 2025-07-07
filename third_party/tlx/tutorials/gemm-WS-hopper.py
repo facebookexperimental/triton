51986fcf578bce7f40722aa9f84785ef6c8fc0e6
dshi7 <shidaohang1990@gmail.com>
2025-07-07 11:04:51 -0700

    M, N, K = (512, 512, 128) BM, BN, BK = (128, 64, 32)

--- third_party/tlx/tutorials/gemm-WS-hopper.py
+++ third_party/tlx/tutorials/gemm-WS-hopper.py
@@ -192,8 +192,8 @@ def test_ws_gemm():
     triton.set_allocator(alloc_fn)
 
     torch.manual_seed(0)
-    # M, N, K = (256, 128, 64)
-    M, N, K = (128, 64, 64)
+    M, N, K = (512, 512, 128)
+    # BM, BN, BK = (128, 64, 32)
 
     a = torch.randn((M, K), dtype=torch.float16, device=DEVICE)
     b = torch.randn((K, N), dtype=torch.float16, device=DEVICE)
