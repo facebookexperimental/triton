72d45cb38b7c45b301d020b1d9fe850cec26ba86
dshi7 <shidaohang1990@gmail.com>
2025-07-12 23:10:12 -0700

    include tlx path in triton cache key
    Test:
    - delete `/tmp/triton_cache_daohang/` entirely
    - run a good unit test
    - introduce bad code changes and run the same unit test

--- python/triton/runtime/cache.py
+++ python/triton/runtime/cache.py
@@ -301,6 +301,12 @@ def triton_key():
     for lib in pkgutil.walk_packages([language_path], prefix="triton.language."):
         with open(lib.module_finder.find_spec(lib.name).origin, "rb") as f:
             contents += [hashlib.sha256(f.read()).hexdigest()]
+    # third-party TLX
+    from pathlib import Path
+    tlx_path = str(Path(TRITON_PATH).parent.parent / "third_party" / "tlx" / "language")
+    for lib in pkgutil.walk_packages([tlx_path], prefix="tlx."):
+        with open(lib.module_finder.find_spec(lib.name).origin, "rb") as f:
+            contents += [hashlib.sha256(f.read()).hexdigest()]
     return f'{__version__}' + '-'.join(contents)
 
 
