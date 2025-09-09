d212d32f05f0b8de8bb204291be4e3fdfbff1999
Pengzhan Zhao <borontion@gmail.com>
2025-09-09 08:52:38 -0700

    [CONFLICTS] Cherry-pick: [AMD][GLUON] Expose WMMA for RDNA3 and RDNA4 (#8111)
    This commit contains merge conflicts that need to be resolved manually.
    Original commit: 4e7dc918494f20807555a6484f31d8811172e5d0
    Original author: Pengzhan Zhao
    
    Please resolve the conflict markers and update this commit.

--- python/triton/experimental/gluon/language/amd/__init__.py
+++ python/triton/experimental/gluon/language/amd/__init__.py
@@ -1,4 +1,9 @@
 from ._layouts import AMDMFMALayout
 from . import cdna3, cdna4
+from . import rdna3, rdna4
 
+<<<<<<< HEAD
 __all__ = ["AMDMFMALayout", "cdna3", "cdna4"]
+=======
+__all__ = ["AMDMFMALayout", "AMDWMMALayout", "cdna3", "cdna4", "rdna3", "rdna4"]
+>>>>>>> 4e7dc9184 ([AMD][GLUON] Expose WMMA for RDNA3 and RDNA4 (#8111))
