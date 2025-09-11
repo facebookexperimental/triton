5657ba1d3d8aa6da674e31906cc0e3d57f804144
dshi7 <shidaohang1990@gmail.com>
2025-09-10 20:08:32 -0700

    fix python/test/unit/cuda/test_experimental_tma.py. some are still broken

--- python/triton/language/core.py
+++ python/triton/language/core.py
@@ -2128,7 +2128,7 @@ def _experimental_reinterpret_tensor_descriptor(desc_ptr, block_shape, dtype, _s
     """
     Reinterpret a generic pointer as a TMA-backed tensor descriptor object.
     """
-    block_ty = block_type(_constexpr_to_value(dtype), block_shape)
+    block_ty = block_type(_unwrap_if_constexpr(dtype), block_shape)
     return _semantic.reinterpret_tensor_descriptor(desc_ptr, block_ty)
 
 
@@ -2152,7 +2152,7 @@ def _experimental_descriptor_store(desc_pointer, value, offsets, store_reduce=""
 
     This stores a tensor of data based on the descriptor and offsets.
     """
-    store_reduce = _constexpr_to_value(store_reduce)
+    store_reduce = _unwrap_if_constexpr(store_reduce)
     desc = _experimental_reinterpret_tensor_descriptor(desc_pointer, value.shape, value.dtype, _semantic=_semantic)
     return desc.store(offsets, value, store_reduce=store_reduce, _semantic=_semantic)
 
