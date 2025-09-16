9db80bb27073723d5e06a881004aa90011a0d173
Nick Riasanovsky <njriasan@meta.com>
2025-09-16 13:52:59 -0700

    Cleaned up the code, need to determine the encoding logic

--- third_party/nvidia/hopper/lib/Transforms/WarpSpecialization/TMEMAlloc1D.cpp
+++ third_party/nvidia/hopper/lib/Transforms/WarpSpecialization/TMEMAlloc1D.cpp
@@ -21,26 +21,62 @@ namespace mlir {
 #define GEN_PASS_DEF_NVGPUTEST1DTMEMALLOC
 #include "nvidia/hopper/include/Transforms/Passes.h.inc"
 
-static mlir::Operation *alloc1DTMEMBuffer(mlir::ModuleOp moduleOp,
-                                          mlir::Operation *producer,
-                                          mlir::Operation *consumer) {
-  return nullptr;
+static ttng::TMEMAllocOp alloc1DTMEMBuffer(OpBuilder &builder,
+                                           Operation *expandedInput) {
+  // Assume we are only every transferring single output ops.
+  auto oldRetType =
+      dyn_cast<RankedTensorType>(expandedInput->getResult(0).getType());
+  if (!oldRetType || oldRetType.getShape().size() != 2) {
+    assert("Producer must be expanded to a 2D Tensor");
+  }
+  SmallVector<int64_t> shape = {oldRetType.getShape().begin(),
+                                oldRetType.getShape().end()};
+  Attribute tensorMemorySpace =
+      triton::nvidia_gpu::TensorMemorySpaceAttr::get(builder.getContext());
+  // TODO: FIXME
+  Attribute encoding = oldRetType.getEncoding();
+  Type accMemDescType = triton::gpu::MemDescType::get(
+      shape, oldRetType.getElementType(), encoding, tensorMemorySpace,
+      /*mutableMemory=*/true);
+  auto allocCall = builder.create<ttng::TMEMAllocOp>(
+      expandedInput->getLoc(), accMemDescType,
+      builder.getType<gpu::AsyncTokenType>(), /*src=*/Value());
+  return allocCall;
 }
 
-static void TMEMStore1D(mlir::Operation *allocOP, mlir::Operation *producer) {
+static ttng::TMEMAllocOp TMEMStore1D(OpBuilder &builder, Operation *producer) {
+  assert(producer->hasAttr("tmem.start") &&
+         "Producer must have tmem.start. We only support 1 producer per "
+         "consumer.");
+
+  // Expand from 1D -> 2D
+  auto producerOutput = producer->getResult(0);
+  auto oldRetType = dyn_cast<RankedTensorType>(producerOutput.getType());
+  if (!oldRetType || oldRetType.getShape().size() != 1) {
+    assert("Producer should only be a 1D Tensor");
+  }
+  builder.setInsertionPoint(producer);
+  auto expandDims = builder.create<triton::ExpandDimsOp>(producer->getLoc(),
+                                                         producerOutput, 1);
+  auto alloc = alloc1DTMEMBuffer(builder, expandDims);
+  // Remove the attribute. There may be multiple users, possibly within
+  // the same partition, so we can't remove the OP entirely.
+  producer->removeAttr("tmem.start");
+  return alloc;
+}
+
+static void TMEMLoad1D(OpBuilder &builder, ttng::TMEMAllocOp allocOP,
+                       Operation *consumer) {
+  assert(consumer->hasAttr("tmem.end") &&
+         "Consumer must have tmem.start. We only support 1 consumer per "
+         "producer.");
   return;
 }
 
-static void TMEMLoad1D(mlir::Operation *allocOP, mlir::Operation *consumer) {
-  return;
-}
-
-static void replaceWith1DTMEM(mlir::ModuleOp moduleOp,
-                              mlir::Operation *producer,
-                              mlir::Operation *consumer) {
-  auto allocOp = alloc1DTMEMBuffer(moduleOp, producer, consumer);
-  TMEMStore1D(allocOp, producer);
-  TMEMLoad1D(allocOp, consumer);
+static void replaceWith1DTMEM(OpBuilder &builder, Operation *producer,
+                              Operation *consumer) {
+  auto allocOp = TMEMStore1D(builder, producer);
+  TMEMLoad1D(builder, allocOp, consumer);
 }
 
 class NVGPUTest1DTMEMAllocPass
@@ -52,8 +88,8 @@ public:
   void runOnOperation() override {
     auto moduleOp = getOperation();
     // Collect all pairs of (tmem.start, tmem.end)
-    mlir::SmallVector<mlir::Operation *> tmemStarts;
-    mlir::SmallVector<mlir::Operation *> tmemEnds;
+    SmallVector<Operation *> tmemStarts;
+    SmallVector<Operation *> tmemEnds;
     moduleOp->walk([&](mlir::Operation *irOp) {
       if (irOp->hasAttr("tmem.start")) {
         auto id =
@@ -74,13 +110,14 @@ public:
     });
     assert(tmemStarts.size() == tmemEnds.size());
     // Generate the actual TMEM operations.
+    OpBuilder builder(moduleOp);
     for (size_t i = 0; i < tmemStarts.size(); i++) {
       auto producer = tmemStarts[i];
       auto consumer = tmemEnds[i];
       assert(producer != nullptr);
       assert(consumer != nullptr);
       // Actual generate the TMEM operations.
-      replaceWith1DTMEM(moduleOp, producer, consumer);
+      replaceWith1DTMEM(builder, producer, consumer);
     }
   }
 };
