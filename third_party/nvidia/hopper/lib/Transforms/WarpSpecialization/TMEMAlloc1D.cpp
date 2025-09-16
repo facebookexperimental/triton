431a1a9e7f5348fcd62c20f96ece2ca393e4c9a0
Nick Riasanovsky <njriasan@meta.com>
2025-09-16 10:07:49 -0700

    Fixed placeholder code

---
+++ third_party/nvidia/hopper/lib/Transforms/WarpSpecialization/TMEMAlloc1D.cpp
@@ -0,0 +1,36 @@
+#include "Utility.h"
+#include "mlir/Analysis/SliceAnalysis.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Transforms/Passes.h"
+#include "nvidia/hopper/include/Transforms/Passes.h"
+#include "triton/Dialect/TritonGPU/IR/Dialect.h"
+#include "triton/Dialect/TritonNvidiaGPU/IR/Dialect.h"
+#include <unordered_set>
+
+#define DEBUG_TYPE "nvgpu-1D-tmem-alloc"
+#define DBGS() (llvm::dbgs() << "[" DEBUG_TYPE "]: ")
+#define LDBG(X) LLVM_DEBUG(DBGS() << X << "\n")
+
+namespace tt = mlir::triton;
+namespace ttg = mlir::triton::gpu;
+namespace ttng = ::mlir::triton::nvidia_gpu;
+namespace mlir {
+
+#define GEN_PASS_DEF_NVGPUTEST1DTMEMALLOC
+#include "nvidia/hopper/include/Transforms/Passes.h.inc"
+
+class NVGPUTest1DTMEMAllocPass
+    : public impl::NVGPUTest1DTMEMAllocBase<NVGPUTest1DTMEMAllocPass> {
+public:
+  using impl::NVGPUTest1DTMEMAllocBase<
+      NVGPUTest1DTMEMAllocPass>::NVGPUTest1DTMEMAllocBase;
+
+  void runOnFuncOp(triton::FuncOp funcOp) {}
+
+  void runOnOperation() override {
+    getOperation()->walk([&](triton::FuncOp funcOp) { runOnFuncOp(funcOp); });
+  }
+};
+
+} // namespace mlir
