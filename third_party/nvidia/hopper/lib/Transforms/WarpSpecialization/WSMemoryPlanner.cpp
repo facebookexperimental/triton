e68339df07aadb3ca0e4144f4a65e755de018dfd
Manman Ren <mren@meta.com>
2025-09-22 12:02:50 -0700

    add memory planner
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

---
+++ third_party/nvidia/hopper/lib/Transforms/WarpSpecialization/WSMemoryPlanner.cpp
@@ -0,0 +1,54 @@
+#include "Utility.h"
+#include "mlir/Analysis/SliceAnalysis.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Transforms/Passes.h"
+#include "nvidia/hopper/include/Transforms/Passes.h"
+#include "triton/Dialect/TritonGPU/IR/Dialect.h"
+#include "triton/Dialect/TritonGPU/Transforms/Utility.h"
+#include "triton/Dialect/TritonNvidiaGPU/IR/Dialect.h"
+
+#define DEBUG_TYPE "nvgpu-ws-memory-planner"
+#define DBGS() (llvm::dbgs() << "[" DEBUG_TYPE "]: ")
+#define LDBG(X) LLVM_DEBUG(DBGS() << X << "\n")
+
+namespace tt = mlir::triton;
+namespace ttg = mlir::triton::gpu;
+namespace ttng = ::mlir::triton::nvidia_gpu;
+namespace mlir {
+
+// Compute a partition schedule for later passes to actually partition the
+// program into async tasks.
+void doMemoryPlanner(triton::FuncOp &funcOp, unsigned numBuffers) {
+
+  // Bail out in the presence of user annotations.
+  DenseSet<int> allAsyncTasks;
+  funcOp->walk([&](Operation *op) {
+    auto asyncTasks = getAsyncTaskIds(op);
+    allAsyncTasks.insert_range(getAsyncTaskIds(op));
+  });
+
+  if (!allAsyncTasks.empty())
+    return;
+}
+
+#define GEN_PASS_DEF_NVGPUTESTWSMEMORYPLANNER
+#include "nvidia/hopper/include/Transforms/Passes.h.inc"
+
+class NVGPUTestWSMemoryPlannerPass
+    : public impl::NVGPUTestWSMemoryPlannerBase<NVGPUTestWSMemoryPlannerPass> {
+public:
+  using impl::NVGPUTestWSMemoryPlannerBase<
+      NVGPUTestWSMemoryPlannerPass>::NVGPUTestWSMemoryPlannerBase;
+
+  void runOnFuncOp(triton::FuncOp funcOp) {
+    if (numBuffers > 1)
+      doMemoryPlanner(funcOp, numBuffers);
+  }
+
+  void runOnOperation() override {
+    getOperation()->walk([&](triton::FuncOp funcOp) { runOnFuncOp(funcOp); });
+  }
+};
+
+} // namespace mlir
