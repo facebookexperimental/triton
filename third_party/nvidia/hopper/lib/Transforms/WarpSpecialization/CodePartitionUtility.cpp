74eae18316d3477adfe7db25b52042a5d21868f3
Nick Riasanovsky <njriasan@meta.com>
2025-10-07 08:23:44 -0700

    Remove flip logic

--- third_party/nvidia/hopper/lib/Transforms/WarpSpecialization/CodePartitionUtility.cpp
+++ third_party/nvidia/hopper/lib/Transforms/WarpSpecialization/CodePartitionUtility.cpp
@@ -907,7 +907,7 @@ typedef struct CommitOpSubgroupInfo CommitOpSubgroupInfo;
 // Check if two values are certain to match given the assumption.
 // that the original value are located in the same block and therefore
 // occur with the same frequency.
-bool valuesMatchOrAlternate(Value v1, Value v2, bool canFlip) {
+bool valuesMatch(Value v1, Value v2) {
   if (v1 == v2) {
     return true;
   }
@@ -919,48 +919,6 @@ bool valuesMatchOrAlternate(Value v1, Value v2, bool canFlip) {
   // Verify the op types match
   if ((op1->getName() != op2->getName()) ||
       (op1->getNumOperands() != op2->getNumOperands())) {
-    // Support barriers with alternating phases. This is a peephole
-    // to check for the XorIOp 1 pattern that we generate to get the
-    // exact opposite phase of the same calculation.
-    //
-    // We only support this if the subsequent op truncates to a single bit
-    // to verify that this is exactly a logical XOR.
-    if (canFlip) {
-      auto xorVal1 = dyn_cast<arith::XOrIOp>(op1);
-      auto xorVal2 = dyn_cast<arith::XOrIOp>(op2);
-      // Both are flipped, something else didn't match.
-      if (xorVal1 && xorVal2) {
-        return false;
-      }
-      auto nonTrueOperand = ([](arith::XOrIOp xorOp) -> int {
-        auto op1 = xorOp.getOperand(0).getDefiningOp();
-        auto op2 = xorOp.getOperand(1).getDefiningOp();
-        if (op1 && isa<arith::ConstantOp>(op1) &&
-            (cast<IntegerAttr>(cast<arith::ConstantOp>(op1).getValue())
-                 .getValue() == 1)) {
-          return 1;
-        }
-        if (op2 && isa<arith::ConstantOp>(op2) &&
-            (cast<IntegerAttr>(cast<arith::ConstantOp>(op2).getValue())
-                 .getValue() == 1)) {
-          return 0;
-        }
-        return -1;
-      });
-      if (xorVal1) {
-        auto keptOperand = nonTrueOperand(xorVal1);
-        if (keptOperand == -1) {
-          return valuesMatchOrAlternate(xorVal1.getOperand(keptOperand), v2,
-                                        false);
-        }
-      } else if (xorVal2) {
-        auto keptOperand = nonTrueOperand(xorVal2);
-        if (keptOperand == -1) {
-          return valuesMatchOrAlternate(v1, xorVal2.getOperand(keptOperand),
-                                        false);
-        }
-      }
-    }
     return false;
   }
 
@@ -969,15 +927,11 @@ bool valuesMatchOrAlternate(Value v1, Value v2, bool canFlip) {
     auto const2 = cast<mlir::arith::ConstantOp>(op2);
     return const1.getValue() == const2.getValue();
   }
-  // We can match the flipping behavior exactly on a truncation
-  // to a single bit.
-  auto truncOp = dyn_cast<arith::TruncIOp>(op1);
-  bool canFlipInput = truncOp && v1.getType().getIntOrFloatBitWidth() == 1;
   // Check all operands
   for (unsigned i = 0; i < op1->getNumOperands(); ++i) {
-    if (!valuesMatchOrAlternate(op1->getOperand(i), op2->getOperand(i),
-                                canFlipInput))
+    if (!valuesMatch(op1->getOperand(i), op2->getOperand(i))) {
       return false;
+    }
   }
   // If all operands match and we have the same exact op type then
   // this op matches.
@@ -989,7 +943,7 @@ bool valuesMatchOrAlternate(Value v1, Value v2, bool canFlip) {
 // every iteration of the loop. If so, then these are safe to fuse.
 bool hasMatchingOrAlternatingPhase(ttng::WaitBarrierOp wait1,
                                    ttng::WaitBarrierOp wait2) {
-  return valuesMatchOrAlternate(wait1.getPhase(), wait2.getPhase(), false);
+  return valuesMatch(wait1.getPhase(), wait2.getPhase());
 }
 
 void mergeSubgroups(std::vector<CommitOpSubgroupInfo> &subgroups, int initCount,
