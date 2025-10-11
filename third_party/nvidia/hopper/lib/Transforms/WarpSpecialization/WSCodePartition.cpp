c31dbf37bab663ae4894e9a8012e1476a814dea0
Hongtao Yu <hoy@fb.com>
2025-10-11 16:04:56 -0700

    Schedule stores

--- third_party/nvidia/hopper/lib/Transforms/WarpSpecialization/WSCodePartition.cpp
+++ third_party/nvidia/hopper/lib/Transforms/WarpSpecialization/WSCodePartition.cpp
@@ -490,8 +490,10 @@ void reorderProducerOps(SmallVector<Channel *> &channels) {
   });
 }
 
-// Reorder producer ops to unblock consumers interleavingly.
-void reorderEpilogOps(triton::FuncOp funcOp) {
+// Reorder operations in epilogs to pack ops on a dependency chain as close as
+// possible.
+void reorderEpilogOps(const SmallVector<Channel *> &channels,
+                      triton::FuncOp funcOp) {
 
   llvm::SetVector<Block *> epliogBlocks;
   funcOp->walk([&](Operation *op) {
@@ -499,6 +501,19 @@ void reorderEpilogOps(triton::FuncOp funcOp) {
       epliogBlocks.insert(op->getBlock());
     }
   });
+
+  auto lastInBlockOperand = [](Operation *op) {
+    Operation *lastOperandOp = nullptr;
+    for (auto opnd : op->getOperands()) {
+      if (auto defOp = opnd.getDefiningOp()) {
+        if (defOp->getBlock() != op->getBlock())
+          continue;
+        if (!lastOperandOp || !defOp->isBeforeInBlock(lastOperandOp))
+          lastOperandOp = defOp;
+      }
+    }
+    return lastOperandOp;
+  };
 
   auto firstInBlockUser = [](Operation *op) {
     Operation *firstUser = nullptr;
@@ -511,7 +526,6 @@ void reorderEpilogOps(triton::FuncOp funcOp) {
     return firstUser;
   };
 
-  // Streamline ops on a store chain
   for (auto block : epliogBlocks) {
     LLVM_DEBUG({
       LDBG("\n");
@@ -521,21 +535,83 @@ void reorderEpilogOps(triton::FuncOp funcOp) {
     });
     // Find the last scf::ForOp in the block
     SetVector<Operation *> epilogOps;
-    SmallVector<Operation *> storeOps;
+    std::map<AsyncTaskId, SmallVector<Operation *>> channelOps;
     for (Operation &op : reverse(*block)) {
-      if (isa<scf::ForOp, scf::IfOp>(op)) {
+      if (isa<scf::ForOp, scf::IfOp>(op))
         break;
-      }
       epilogOps.insert(&op);
-      if (isa<tt::DescriptorStoreOp, tt::StoreOp>(op))
-        storeOps.push_back(&op);
     }
 
-    for (auto storeOp : reverse(storeOps)) {
-      // Move forward dependencies close to the op
-      // Start from the last store op backwards and move backward slice to
-      // before each op. This guarantees that the backward slice of each op is
-      // scheduled as late as possible.
+    for (auto channel : channels) {
+      if (epilogOps.contains(channel->getDstOp()))
+        channelOps[channel->relation.first].push_back(channel->getDstOp());
+    }
+
+    // Streamline ops on a channel chain.
+    // Starting with producers with smaller task ids, moving forward
+    // dependencies of the consumer ops close to the them.
+    for (auto item : channelOps) {
+      for (auto op : item.second) {
+        SetVector<Operation *> forwardSlice;
+        (void)getForwardSlice(op, &forwardSlice);
+        for (auto &depOp : reverse(forwardSlice)) {
+          if (!epilogOps.contains(depOp))
+            continue;
+          // push depOp to be right after its operands
+          auto lastOpndOp = lastInBlockOperand(depOp);
+          if (lastOpndOp)
+            depOp->moveAfter(lastOpndOp);
+        }
+      }
+    }
+
+    // Group store ops based on types.
+    SmallVector<SmallVector<Operation *, 2>, 2> storeBuckets(2);
+    for (auto op : reverse(epilogOps)) {
+      if (isa<tt::DescriptorStoreOp>(op))
+        storeBuckets[0].push_back(op);
+      if (isa<tt::StoreOp>(op))
+        storeBuckets[1].push_back(op);
+    }
+
+    // Reorder store operations in the sequence:
+    //   bucket[0][N], bucket[1][N],
+    //   bucket[0][N-1], bucket[1][N-1],
+    //   ...
+    //   bucket[0][0], bucket[1][0].
+    //
+    // This ordering aligns with the expected producer pattern, where
+    // producers of bucket[0][0], bucket[1][0], ... complete earlier than
+    // those of bucket[0][1], bucket[1][1], and so on. By reordering the
+    // stores in this manner, we ensure that operations finish as early as
+    // possible overall.
+    SmallVector<Operation *> storeOps;
+    bool changed = true;
+    while (changed) {
+      changed = false;
+      for (auto &store : storeBuckets) {
+        if (!store.empty()) {
+          storeOps.push_back(store.back());
+          store.pop_back();
+          changed = true;
+        }
+      }
+    }
+
+    assert(storeBuckets[0].empty() && storeBuckets[1].empty() &&
+           "All stores must have been processed");
+
+    // Reorder stores op physically based on the computed order.
+    for (unsigned i = 1; i < storeOps.size(); i++) {
+      storeOps[i]->moveBefore(storeOps[i - 1]);
+    }
+
+    // Streamline ops on a store chain
+    // For each store op, move backward dependencies close to the op.
+    // Start from the last store op backwards and move backward slice to
+    // before each op. This guarantees that the backward slice of each op is
+    // scheduled as late as possible.
+    for (auto storeOp : storeOps) {
       BackwardSliceOptions opt;
       opt.omitBlockArguments = true;
       SetVector<Operation *> backwardSlice;
@@ -545,7 +621,8 @@ void reorderEpilogOps(triton::FuncOp funcOp) {
           continue;
         // push depOp to be right before its first user
         auto firstUser = firstInBlockUser(depOp);
-        depOp->moveBefore(firstUser);
+        if (firstUser)
+          depOp->moveBefore(firstUser);
       }
     }
 
@@ -2527,7 +2604,6 @@ static void cleanupTmemTokens(triton::FuncOp funcOp) {
 }
 
 void doBufferAllocation(triton::FuncOp &funcOp) {
-  reorderEpilogOps(funcOp);
   // Step 1: collect all communications between producers and consumers.
   SmallVector<std::unique_ptr<Channel>> channelsOrigin;
   collectAsyncChannels(channelsOrigin, funcOp, 1 /*numBuffers*/);
@@ -2539,7 +2615,10 @@ void doBufferAllocation(triton::FuncOp &funcOp) {
     return;
   }
 
-  // Step 2: Create buffers. A buffer for each channel.
+  // Step 2: Reorder ops based on channel information.
+  reorderEpilogOps(channels, funcOp);
+
+  // Step 3: Create buffers. A buffer for each channel.
   createBuffer(channels, funcOp);
 }
 
