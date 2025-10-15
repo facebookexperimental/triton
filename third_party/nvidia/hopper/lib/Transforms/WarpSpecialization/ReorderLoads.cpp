76d9c61ea3ce82cf11da4b4e9b2c6d3e7beab734
Nick Riasanovsky <njriasan@meta.com>
2025-10-14 18:30:15 -0700

    Add mma priority logic

--- third_party/nvidia/hopper/lib/Transforms/WarpSpecialization/ReorderLoads.cpp
+++ third_party/nvidia/hopper/lib/Transforms/WarpSpecialization/ReorderLoads.cpp
@@ -1,15 +1,49 @@
 #include "ReorderLoads.h"
-#include "triton/Dialect/TritonGPU/IR/Dialect.h"
+#include "triton/Dialect/TritonGPU/Transforms/PipeliningUtility.h"
 #include "triton/Dialect/TritonNvidiaGPU/IR/Dialect.h"
 
 namespace tt = mlir::triton;
-namespace ttg = ::mlir::triton::gpu;
 namespace ttng = ::mlir::triton::nvidia_gpu;
 
 namespace mlir {
 
 DenseMap<ttng::MMAv5OpInterface, int> determineMMAPriority(tt::FuncOp &funcOp) {
   DenseMap<ttng::MMAv5OpInterface, int> mmaPriority;
+  // Collect all the MMA ops. Right now we require every MMA op be annotated
+  // with LoopSchedule information.
+  SmallVector<ttng::MMAv5OpInterface> allMMAs;
+  bool isSupported = true;
+  funcOp.walk([&](ttng::MMAv5OpInterface mmaOp) {
+    if (!mmaOp->hasAttr(tt::kLoopStageAttrName) ||
+        !mmaOp->hasAttr(tt::kLoopClusterAttrName)) {
+      isSupported = false;
+      return;
+    }
+    allMMAs.push_back(mmaOp);
+  });
+  llvm::sort(allMMAs, [&](ttng::MMAv5OpInterface a, ttng::MMAv5OpInterface b) {
+    if (a->getAttrOfType<IntegerAttr>(tt::kLoopStageAttrName).getInt() <
+        b->getAttrOfType<IntegerAttr>(tt::kLoopStageAttrName).getInt()) {
+      return true;
+    } else if (a->getAttrOfType<IntegerAttr>(tt::kLoopClusterAttrName)
+                   .getInt() <
+               b->getAttrOfType<IntegerAttr>(tt::kLoopStageAttrName).getInt()) {
+      return true;
+    } else {
+      if (a->getBlock() != b->getBlock()) {
+        isSupported = false;
+        // Doesn't matter. Will abort.
+        return true;
+      } else {
+        return a->isBeforeInBlock(b);
+      }
+    }
+  });
+  if (isSupported) {
+    for (int i = 0; i < allMMAs.size(); ++i) {
+      mmaPriority[allMMAs[i]] = i;
+    }
+  }
   return mmaPriority;
 }
 
