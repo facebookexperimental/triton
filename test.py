c8eded6394d5587649819bcee93e6ea9f33c79e6
Hongtao Yu <hoy@fb.com>
2025-11-07 14:58:48 -0800

    rename kernel

--- test.py
+++ test.py
@@ -44,7 +44,9 @@ empty_strided_p2p = torch._C._distributed_c10d._SymmetricMemory.empty_strided_p2
 #   %_low_memory_max_pool_offsets_to_indices : Tensor "i64[1, 2, 2, 4][16, 1, 8, 2]cuda:0"[num_users=1] = call_function[target=torch.ops.prims._low_memory_max_pool_offsets_to_indices.default](args = (%getitem_1, [3, 3], [3, 6], [2, 2], [1, 1], [1, 1]), kwargs = {})
 #   %max_pool2d_with_indices_backward : Tensor "f32[1, 2, 3, 6][36, 1, 12, 2]cuda:0"[num_users=1] = call_function[target=torch.ops.aten.max_pool2d_with_indices_backward.default](args = (%tangents_1, %primals_1, [3, 3], [2, 2], [1, 1], [1, 1], True, %_low_memory_max_pool_offsets_to_indices), kwargs = {})
 #   return %max_pool2d_with_indices_backward
-triton_poi_fused_max_pool2d_with_indices_max_pool2d_with_indices_backward_0 = async_compile.triton('triton_poi_fused_max_pool2d_with_indices_max_pool2d_with_indices_backward_0', '''
+triton_kernel = async_compile.triton(
+    "triton_kernel",
+    """
 import triton
 import triton.language as tl
 
@@ -57,11 +59,11 @@ triton_helpers.set_driver_to_gpu()
     size_hints={'x': 64},
     filename=__file__,
     triton_meta={'signature': {'in_ptr0': '*i8', 'in_ptr1': '*fp32', 'out_ptr0': '*fp32', 'xnumel': 'i32', 'XBLOCK': 'constexpr'}, 'device': DeviceProperties(type='cuda', index=0, multi_processor_count=132, cc=90, major=9, regs_per_multiprocessor=65536, max_threads_per_multi_processor=2048, warp_size=32), 'constants': {}, 'native_matmul': False, 'configs': [{(0,): [['tt.divisibility', 16]], (1,): [['tt.divisibility', 16]], (2,): [['tt.divisibility', 16]]}], 'enable_fp_fusion': True},
-    inductor_meta={'grid_type': 'Grid1D', 'autotune_hints': set(), 'kernel_name': 'triton_poi_fused_max_pool2d_with_indices_max_pool2d_with_indices_backward_0', 'mutated_arg_names': [], 'optimize_mem': True, 'no_x_dim': False, 'atomic_add_found': False, 'num_load': 8, 'num_store': 1, 'num_reduction': 0, 'backend_hash': '43BC6A92279F7119AB953E3C1B0818031BDC768AC57E822187055CE52B5863C4', 'assert_indirect_indexing': True, 'autotune_local_cache': True, 'autotune_pointwise': False, 'autotune_remote_cache': None, 'force_disable_caches': False, 'dynamic_scale_rblock': True, 'max_autotune': False, 'max_autotune_pointwise': False, 'min_split_scan_rblock': 256, 'spill_threshold': 16, 'store_cubin': False, 'deterministic': False, 'force_filter_reduction_configs': False, 'are_deterministic_algorithms_enabled': False},
+    inductor_meta={'grid_type': 'Grid1D', 'autotune_hints': set(), 'kernel_name': 'triton_kernel', 'mutated_arg_names': [], 'optimize_mem': True, 'no_x_dim': False, 'atomic_add_found': False, 'num_load': 8, 'num_store': 1, 'num_reduction': 0, 'backend_hash': '43BC6A92279F7119AB953E3C1B0818031BDC768AC57E822187055CE52B5863C4', 'assert_indirect_indexing': True, 'autotune_local_cache': True, 'autotune_pointwise': False, 'autotune_remote_cache': None, 'force_disable_caches': False, 'dynamic_scale_rblock': True, 'max_autotune': False, 'max_autotune_pointwise': False, 'min_split_scan_rblock': 256, 'spill_threshold': 16, 'store_cubin': False, 'deterministic': False, 'force_filter_reduction_configs': False, 'are_deterministic_algorithms_enabled': False},
     min_elem_per_thread=0
 )
 @triton.jit
-def triton_poi_fused_max_pool2d_with_indices_max_pool2d_with_indices_backward_0(in_ptr0, in_ptr1, out_ptr0, xnumel, XBLOCK : tl.constexpr):
+def triton_kernel(in_ptr0, in_ptr1, out_ptr0, xnumel, XBLOCK : tl.constexpr):
     xnumel = 36
     xoffset = tl.program_id(0) * XBLOCK
     xindex = xoffset + tl.arange(0, XBLOCK)[:]
@@ -232,7 +234,9 @@ def triton_poi_fused_max_pool2d_with_indices_max_pool2d_with_indices_backward_0(
     tl.static_assert(tmp57.dtype == tl.float32)
     tl.static_assert(tmp57.dtype == tl.float32)
     tl.store(out_ptr0 + (x5), tmp57, xmask)
-''', device_str='cuda')
+""",
+    device_str="cuda",
+)
 
 
 async_compile.wait(globals())
@@ -264,9 +268,9 @@ class Runner:
             torch.cuda.set_device(0)
             buf0 = empty_strided_cuda((1, 2, 3, 6), (36, 1, 12, 2), torch.float32)
             # Topologically Sorted Source Nodes: [max_pool2d], Original ATen: [aten.max_pool2d_with_indices, aten.max_pool2d_with_indices_backward]
-            # [Provenance debug handles] triton_poi_fused_max_pool2d_with_indices_max_pool2d_with_indices_backward_0:229
+            # [Provenance debug handles] triton_kernel:229
             stream0 = get_raw_stream(0)
-            triton_poi_fused_max_pool2d_with_indices_max_pool2d_with_indices_backward_0.run(getitem_1, tangents_1, buf0, 36, stream=stream0)
+            triton_kernel.run(getitem_1, tangents_1, buf0, 36, stream=stream0)
             del getitem_1
             del tangents_1
         # torch.save(buf0, "out.pt")
