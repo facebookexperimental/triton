de46db931f8808e375eda2d9f29f5476904080ed
Nick Riasanovsky <njriasan@meta.com>
2025-11-26 22:37:01 -0800

    Fix the implementation

--- third_party/tlx/tutorials/blackwell-fa-ws-pipelined-persistent_test.py
+++ third_party/tlx/tutorials/blackwell-fa-ws-pipelined-persistent_test.py
@@ -238,8 +238,8 @@ def _softmax_inner_loop(
         # -- compute correction factor
         alpha = tl.math.exp2(m_i - m_ij)
         tlx.barrier_wait(tlx.local_view(alpha_empties, cid), qk_phase ^ 1)
-        # Use alpha[0] for cid=0, and alpha[HEAD_DIM] for cid=1
-        tlx.local_store(tlx.local_view(alpha_tiles, cid * HEAD_DIM), alpha[:, None])
+        # Use alpha[0] for cid=0, and alpha[BLOCK_N] for cid=1
+        tlx.local_store(tlx.local_view(alpha_tiles, cid * BLOCK_N), alpha[:, None])
         tlx.barrier_arrive(tlx.local_view(alpha_fulls, cid))
 
         qk = _fma_f32x2(qk, qk_scale, -m_ij[:, None])
@@ -381,8 +381,8 @@ def _attn_fwd_ws(sm_scale, M,  #
                     for cid in tl.static_range(0, NUM_MMA_GROUPS):
                         # -- update output accumulator --
                         tlx.barrier_wait(alpha_fulls[cid], phase)
-                        # Use alpha[0] for cid=0, and alpha[HEAD_DIM] for cid=1
-                        alpha_1 = tlx.local_load(alpha_tiles[cid * HEAD_DIM])
+                        # Use alpha[0] for cid=0, and alpha[BLOCK_N] for cid=1
+                        alpha_1 = tlx.local_load(alpha_tiles[cid * BLOCK_N])
                         tlx.barrier_arrive(alpha_empties[cid])
                         for slice_id in tl.static_range(0, NUM_MMA_SLICES):
                             subslice = tlx.subslice(
@@ -401,11 +401,11 @@ def _attn_fwd_ws(sm_scale, M,  #
                 for cid in tl.static_range(0, NUM_MMA_GROUPS):
                     # epilogue
                     tlx.barrier_wait(l_fulls[cid], phase)
-                    # Use l[1]/l[1+HEAD_DIM] and m[2][2 + HEAD_DIM]
-                    # to disambigulate from alpha[0]/alpha[HEAD_DIM]
-                    l = tlx.local_load(l_tiles[cid * HEAD_DIM + 1])
+                    # Use l[1]/l[1+BLOCK_N] and m[2][2 + BLOCK_N]
+                    # to disambigulate from alpha[0]/alpha[BLOCK_N]
+                    l = tlx.local_load(l_tiles[cid * BLOCK_N + 1])
                     tlx.barrier_arrive(qk_empties[cid])
-                    m = tlx.local_load(m_tiles[cid * HEAD_DIM + 2])
+                    m = tlx.local_load(m_tiles[cid * BLOCK_N + 2])
                     m += tl.math.log2(l)
                     offs_m = (start_m * BLOCK_M + cid * BLOCK_M_SPLIT + tl.arange(0, BLOCK_M_SPLIT))
                     m_ptrs = M + off_hz * N_CTX + offs_m
@@ -503,10 +503,10 @@ def _attn_fwd_ws(sm_scale, M,  #
                     )
 
                 # prepare l_i for the epilog
-                # Use l[1]/l[1+HEAD_DIM] and m[2][2 + HEAD_DIM]
-                # to disambigulate from alpha[0]/alpha[HEAD_DIM]
-                tlx.local_store(l_tiles[cid * HEAD_DIM + 1], l_i[:, None])
-                tlx.local_store(m_tiles[cid * HEAD_DIM + 2], m_i[:, None])
+                # Use l[1]/l[1+BLOCK_N] and m[2][2 + BLOCK_N]
+                # to disambigulate from alpha[0]/alpha[BLOCK_N]
+                tlx.local_store(l_tiles[cid * BLOCK_N + 1], l_i[:, None])
+                tlx.local_store(m_tiles[cid * BLOCK_N + 2], m_i[:, None])
                 tlx.barrier_arrive(l_fulls[cid])
                 tile_idx += num_progs
 
