e09c8f876190c78cf2c2bb8dfe4cf575ee56286b
Nick Riasanovsky <njriasan@meta.com>
2025-12-02 17:29:30 -0800

    Fixed the local view issue

--- third_party/tlx/tutorials/blackwell-fa-ws-pipelined-persistent_test.py
+++ third_party/tlx/tutorials/blackwell-fa-ws-pipelined-persistent_test.py
@@ -1145,9 +1145,9 @@ def _bwd_compute_inner_loop(
         m = tl.load(M + offs_m)
 
         # wait for qkT = tl.dot(k, qT)
-        tlx.barrier_wait(qk_fulls[tmem_buf_id], tmem_phase)
-        qkT = tlx.local_load(qk_tiles[tmem_buf_id])
-        tlx.barrier_arrive(qk_empties[tmem_buf_id])
+        tlx.barrier_wait(tlx.local_view(qk_fulls, tmem_buf_id), tmem_phase)
+        qkT = tlx.local_load(tlx.local_view(qk_tiles, tmem_buf_id))
+        tlx.barrier_arrive(tlx.local_view(qk_empties, tmem_buf_id))
 
         pT = tl.math.exp2(qkT - m[None, :])
         if STAGE == 1:
@@ -1157,22 +1157,22 @@ def _bwd_compute_inner_loop(
         # ppT *= qk_scale
         ppT = pT
         ppT = ppT.to(do_out_dtype)
-        tlx.local_store(p_tiles[tmem_buf_id], ppT)
-        tlx.barrier_arrive(p_fulls[tmem_buf_id])
+        tlx.local_store(tlx.local_view(p_tiles, tmem_buf_id), ppT)
+        tlx.barrier_arrive(tlx.local_view(p_fulls, tmem_buf_id))
 
         # D (= delta) is pre-divided by ds_scale.
         Di = tl.load(D + offs_m)
 
         # Wait for dpT = tl.dot(v, tl.trans(do))
-        tlx.barrier_wait(dp_fulls[tmem_buf_id], tmem_phase)
-        dpT = tlx.local_load(dp_tiles[tmem_buf_id])
+        tlx.barrier_wait(tlx.local_view(dp_fulls, tmem_buf_id), tmem_phase)
+        dpT = tlx.local_load(tlx.local_view(dp_tiles, tmem_buf_id))
         # No need to release dP, as dP uses the same tmem as dQ
         # in the same iteration. Release dQ instead later.
         dsT = pT * (dpT - Di[None, :])
         dsT = dsT.to(q_out_dtype)
-        tlx.local_store(ds_tiles[ds_buf_id], dsT)
+        tlx.local_store(tlx.local_view(ds_tiles, ds_buf_id), dsT)
         tlx.fence_async_shared()
-        tlx.barrier_arrive(ds_fulls[ds_buf_id])
+        tlx.barrier_arrive(tlx.local_view(ds_fulls, ds_buf_id))
         curr_m += step_m
 
 
