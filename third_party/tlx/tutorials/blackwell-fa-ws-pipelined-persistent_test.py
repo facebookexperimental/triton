4097a638bb6011c895ef963959fb59e850bdbbe1
Nick Riasanovsky <njriasan@meta.com>
2025-12-04 15:05:19 -0800

    Fix other bugs

--- third_party/tlx/tutorials/blackwell-fa-ws-pipelined-persistent_test.py
+++ third_party/tlx/tutorials/blackwell-fa-ws-pipelined-persistent_test.py
@@ -1427,7 +1427,7 @@ def _attn_bwd_ws(
                         [(off_bh + start_n).to(tl.int32), slice_id * slice_size],
                         dv.to(tlx.dtype_of(desc_dv)),
                     )
-                tlx.async_descriptor_store_wait(0)
+                    tlx.async_descriptor_store_wait(0)
                 tlx.barrier_arrive(dv_empties[kv_buf_id])
                 tlx.barrier_wait(dk_fulls[kv_buf_id], kv_phase)
                 for slice_id in tl.static_range(EPILOGUE_SUBTILE):
@@ -1497,9 +1497,9 @@ def _attn_bwd_ws(
 
                 # Compute dpT = tl.dot(v, tl.trans(do))
                 tlx.barrier_wait(do_fulls[do_buf_id], do_phase)
-                # TODO: Optimize the
+                # # TODO: Optimize the
                 tlx.barrier_wait(dv_empties[kv_buf_id], kv_phase ^ 1)
-                tlx.barrier_wait(dk_empties[kv_buf_id], kv_phase ^ 1)
+                # tlx.barrier_wait(dk_empties[kv_buf_id], kv_phase ^ 1)
                 # As dP uses the same tmem as dQ, wait for dQ release.
                 tlx.barrier_wait(dq_empties[tmem_buf_id], tmem_phase ^ 1)
                 doT = tlx.local_trans(do_tiles[do_buf_id])
@@ -1516,7 +1516,7 @@ def _attn_bwd_ws(
                 tlx.async_dot(
                     p_tiles[tmem_buf_id],
                     do_tiles[do_buf_id],
-                    dv_tiles[tmem_buf_id],
+                    dv_tiles[kv_buf_id],
                     use_acc=False,
                     mBarriers=[do_empties[do_buf_id]],
                 )
@@ -1567,7 +1567,7 @@ def _attn_bwd_ws(
                     tlx.async_dot(
                         ds_tiles[ds_buf_id_prev],
                         q_tiles[q_buf_id_prev],
-                        dk_tiles[tmem_buf_id_prev],
+                        dk_tiles[kv_buf_id],
                         use_acc=(i - 1) > 0,
                         mBarriers=[
                             q_empties[q_buf_id_prev],
@@ -1593,7 +1593,7 @@ def _attn_bwd_ws(
                     tlx.async_dot(
                         p_tiles[tmem_buf_id],
                         do_tiles[do_buf_id],
-                        dv_tiles[tmem_buf_id],
+                        dv_tiles[kv_buf_id],
                         use_acc=True,
                         mBarriers=[do_empties[do_buf_id]],
                     )
@@ -1615,12 +1615,9 @@ def _attn_bwd_ws(
                 tlx.async_dot(
                     ds_tiles[ds_buf_id],
                     q_tiles[q_buf_id],
-                    dk_tiles[tmem_buf_id],
+                    dk_tiles[kv_buf_id],
                     use_acc=num_steps > 1,
-                    mBarriers=[
-                        q_empties[q_buf_id],
-                        dk_fulls[kv_buf_id],
-                    ],
+                    mBarriers=[q_empties[q_buf_id], dk_fulls[tmem_buf_id]],
                 )
 
                 # Compute dq = tl.dot(tl.trans(dsT), k)
