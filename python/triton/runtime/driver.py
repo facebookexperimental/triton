065798eb550384acb5c0d9b2d63cb17ab1d02a7e
Agron Tsai <agrontsai@meta.com>
2026-01-12 15:07:37 -0800

    [triton][beta] Triton 3.5+fb: Add activer setter/deleter in driver config for beta (#772)
    Summary:
    
    TSIA
    Same change one stable D87191079
    
    Reviewed By: dshi7
    
    Differential Revision: D87255530

--- python/triton/runtime/driver.py
+++ python/triton/runtime/driver.py
@@ -6,12 +6,13 @@ from ..backends import backends, DriverBase
 def _create_driver() -> DriverBase:
     active_drivers = [x.driver for x in backends.values() if x.driver.is_active()]
     if len(active_drivers) != 1:
-        raise RuntimeError(f"{len(active_drivers)} active drivers ({active_drivers}). There should only be one.")
+        raise RuntimeError(
+            f"{len(active_drivers)} active drivers ({active_drivers}). There should only be one."
+        )
     return active_drivers[0]()
 
 
 class DriverConfig:
-
     def __init__(self) -> None:
         self._default: DriverBase | None = None
         self._active: DriverBase | None = None
@@ -33,6 +34,21 @@ class DriverConfig:
 
     def reset_active(self) -> None:
         self._active = self.default
+
+    # Facebook begin
+    # add setter and deleter for active property
+    # to unblock internal use case of setting patch
+    # with patch("xxx.triton.runtime.driver.active")
+    # otherwise we can revert https://github.com/triton-lang/triton/pull/7770
+    @active.setter
+    def active(self, value):
+        self._active = value
+
+    @active.deleter
+    def active(self):
+        self._active = None
+
+    # Facebook end
 
 
 driver = DriverConfig()
