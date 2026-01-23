c04032bc81748e37418b19dbdc7958169a3e3789
Nick Riasanovsky <njriasan@meta.com>
2026-01-22 16:51:36 -0800

    [Triton] [GB300] [Beta] Add a pass to warn about FP64 performance (#792)
    Summary:
    Pull Request resolved: https://github.com/facebookexperimental/triton/pull/792
    
    Adds a pass to warn about FP64 performance on GB300.
    
    Reviewed By: manman-ren, htyu
    
    Differential Revision: D91165933
    
    fbshipit-source-id: b4bd13501b9d041af79ce3e00dd4f7b8c2c7238a

--- include/triton/Dialect/Triton/Transforms/Passes.h
+++ include/triton/Dialect/Triton/Transforms/Passes.h
@@ -3,12 +3,30 @@
 
 #include "mlir/Pass/Pass.h"
 
+#include <memory>
+#include <string>
+#include <vector>
+
 namespace mlir {
+
+class ModuleOp;
+template <typename T> class OperationPass;
+
 namespace triton {
 
 // Generate the pass class declarations.
 #define GEN_PASS_DECL
 #include "triton/Dialect/Triton/Transforms/Passes.h.inc"
+
+std::unique_ptr<OperationPass<ModuleOp>>
+createCudaWarningsPass(int32_t computeCapability);
+
+/// Collect CUDA-specific performance warnings for a module.
+/// Returns a vector of warning messages that can be used to populate Python
+/// warnings. The pass version (createCudaWarningsPass) also emits these as
+/// MLIR warnings for lit testing purposes.
+std::vector<std::string> collectCudaWarnings(ModuleOp module,
+                                             int32_t computeCapability);
 
 #define GEN_PASS_REGISTRATION
 #include "triton/Dialect/Triton/Transforms/Passes.h.inc"
