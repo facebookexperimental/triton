be6d64c8effbf80b3b6d21a2efcbb609162fb2a5
Nick Riasanovsky <njriasan@meta.com>
2026-02-03 15:47:38 -0800

    Removed dead code

--- third_party/tlx/tutorials/blackwell-fa-ws-pipelined-persistent_mxfp8_test.py
+++ third_party/tlx/tutorials/blackwell-fa-ws-pipelined-persistent_mxfp8_test.py
@@ -350,29 +350,6 @@ def _get_unfused_loop_bounds(start_m, N_CTX, BLOCK_M, STAGE: tl.constexpr):
 
 
 @triton.jit
-def _get_start_m_bwd(start_n, BLOCK_N1, STAGE: tl.constexpr):
-    if STAGE == 1:
-        return 0
-    else:
-        tl.static_assert(STAGE == 3)
-        return start_n * BLOCK_N1
-
-
-@triton.jit
-def _get_unfused_bwd_loop_bounds(start_n, N_CTX, BLOCK_N1, STAGE: tl.constexpr):
-    if STAGE == 1:
-        # First part of STAGE == 3
-        lo, hi = start_n * BLOCK_N1, (start_n + 1) * BLOCK_N1
-    elif STAGE == 2:
-        # Second part of STAGE == 3 in this function
-        lo, hi = (start_n + 1) * BLOCK_N1, N_CTX
-    else:
-        tl.static_assert(STAGE == 3)
-        lo, hi = 0, N_CTX
-    return lo, hi
-
-
-@triton.jit
 def _get_fused_loop_bounds(start_m, N_CTX, BLOCK_M, STAGE: tl.constexpr):
     if STAGE == 1:
         return 0, N_CTX
