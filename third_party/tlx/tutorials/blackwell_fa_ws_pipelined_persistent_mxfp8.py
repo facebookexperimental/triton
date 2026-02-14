6f6f73d559ed43d3c0e1adc97309c6a85d441268
Nick Riasanovsky <njriasan@meta.com>
2026-02-13 22:54:38 -0800

    move to commit

--- third_party/tlx/tutorials/blackwell_fa_ws_pipelined_persistent_mxfp8.py
+++ third_party/tlx/tutorials/blackwell_fa_ws_pipelined_persistent_mxfp8.py
@@ -600,6 +600,7 @@ def _attn_fwd_mxf8_ws(sm_scale, M,  #
                 tlx.barrier_wait(kv_scale_fulls[k_bufIdx], k_phase)
                 # Explicit SMEM->TMEM scale transfer
                 tlx.tmem_copy(q_scale_tiles[0], q_scale_tmem[q_tmem_base])
+                tlx.tcgen05_commit(q_scale_empties[q_bufIdx])
                 tlx.tmem_copy(kv_scale_tiles[k_bufIdx], k_scale_tmem[kv_tmem_idx])
                 tlx.async_dot_scaled(
                     q_tiles[0],
@@ -610,7 +611,7 @@ def _attn_fwd_mxf8_ws(sm_scale, M,  #
                     k_scale_tmem[kv_tmem_idx],
                     K_FP8_FORMAT,
                     use_acc=False,
-                    mBarriers=[qk_fulls[0], q_scale_empties[q_bufIdx]],
+                    mBarriers=[qk_fulls[0]],
                 )
 
                 # -- compute q1 @ k ----
@@ -620,6 +621,7 @@ def _attn_fwd_mxf8_ws(sm_scale, M,  #
                 tlx.barrier_wait(q_scale_fulls[q_bufIdx + NUM_BUFFERS_Q], q_phase)
                 # Explicit SMEM->TMEM scale transfer
                 tlx.tmem_copy(q_scale_tiles[1], q_scale_tmem[q_tmem_base + 1])
+                tlx.tcgen05_commit(q_scale_empties[q_bufIdx + NUM_BUFFERS_Q])
                 tlx.async_dot_scaled(
                     q_tiles[1],
                     k_tile,
@@ -633,7 +635,6 @@ def _attn_fwd_mxf8_ws(sm_scale, M,  #
                         qk_fulls[1],
                         kv_empties[k_bufIdx],
                         kv_scale_empties[k_bufIdx],
-                        q_scale_empties[q_bufIdx + NUM_BUFFERS_Q],
                     ],
                 )
 
