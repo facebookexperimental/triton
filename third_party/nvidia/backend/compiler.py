51ff0eeeaabb176b0a9c7d0ffaa9a813197616f0
Agron Tsai <agrontsai@meta.com>
2026-02-14 22:14:46 -0800

    [triton][beta] [Cherry-pick][RESOLVED] '[BACKEND] Retain mlir reproducer temporaries from prior run pass pipelines (#8113)' (#906)
    Summary:
    Pull Request resolved: https://github.com/facebookexperimental/triton/pull/906
    
    This is a cherry-pick of an upstream PR: https://github.com/triton-lang/triton/pull/8113
    
    Upstream commit message:
    ```
    > [BACKEND] Retain mlir reproducer temporaries from prior run pass pipelines (#8113)
    
    > Currently MLIR reproducers for each pass pipeline run overrides the
    > previous `TRITON_REPRODUCER_PATH` path. This change allows for including
    > a reproducer suffix when calling pm.run() to allow for retaining all
    > previously run pipeline reproducers prior to the most recently run pass
    > pipeline.
    
    > This is important to add because with multiple pipelines, it is
    > necessary to retain all previous pipelines reproducers to reproduce the
    > full compilation sequence.
    
    > - [X] I am not making a trivial change, such as fixing a typo in a
    > comment.
    
    > - [X] I have written a PR description following these
    >   [rules](https://cbea.ms/git-commit/#why-not-how).
    
    > - [X] I have run `pre-commit run --from-ref origin/main --to-ref HEAD`.
    
    > - Select one of the following.
    >   - [X] I have added tests.
    >     - `/python/test` for end-to-end tests
    
    > - Select one of the following.
    >   - [X] I have not added any `lit` tests.
    ```
    
    Conflict Resolution:
    - File: third_party/nvidia/backend/compiler.py:385-390
      Action: Took upstream change - added 'make_ttgir' argument to pm.run()
      Reason: This is the feature being cherry-picked (reproducer naming)
    
    - File: third_party/nvidia/backend/compiler.py:451-458
      Action: Took only pm.run(mod, 'make_llir') change, discarded duplicate llvm.init_targets()/context lines
      Reason: The additional lines already exist later in the function; only the reproducer naming is needed
    
    ***Do not remove the following line from this commit***
    Reactor Cherry-pick Revision: 625c8cb4545c78c3ee753e5bb68198551c72b24d
     ---
    
    This diff was generated by running:
    ```
    buck run fbcode//triton/tools/reactor:reactor -- cherrypick --num-commits 1
    ```
    
    Reviewed By: stashuk-olek
    
    Differential Revision: D93144413
    
    fbshipit-source-id: 6e79f1c9482c8725b7cc78874f90815c3ff3651b

--- third_party/nvidia/backend/compiler.py
+++ third_party/nvidia/backend/compiler.py
@@ -271,7 +271,7 @@ class CUDABackend(BaseBackend):
         passes.common.add_cse(pm)
         passes.common.add_symbol_dce(pm)
         passes.ttir.add_loop_unroll(pm)
-        pm.run(mod)
+        pm.run(mod, 'make_ttir')
         return mod
 
     @staticmethod
@@ -382,8 +382,7 @@ class CUDABackend(BaseBackend):
         passes.common.add_cse(pm)
         passes.common.add_canonicalizer(pm)
 
-        pm.run(mod)
-
+        pm.run(mod, 'make_ttgir')
         metadata["cluster_dims"] = (cluster_info.clusterDimX, cluster_info.clusterDimY, cluster_info.clusterDimZ)
         # Track whether ctas_per_cga was explicitly set to distinguish between
         # Triton's way (num_ctas > 1) and TLX/CUDA way (ctas_per_cga set).
@@ -404,7 +403,7 @@ class CUDABackend(BaseBackend):
         passes.gluon.add_canonicalizer(pm)
         passes.ttgpuir.add_combine_tensor_select_and_if(pm)
 
-        pm.run(mod)
+        pm.run(mod, 'gluon_to_ttgir')
         metadata["tensordesc_meta"] = mod.get_tensordesc_metadata()
         return mod
 
@@ -449,7 +448,7 @@ class CUDABackend(BaseBackend):
         if CUDABackend.instrumentation:
             CUDABackend.instrumentation.patch("llvmir_to_llvm", pm, mod.context)
 
-        pm.run(mod)
+        pm.run(mod, 'make_llir')
 
         if knobs.compilation.dump_ir_extract_di_local_variables:
             # comments below on why separate it
