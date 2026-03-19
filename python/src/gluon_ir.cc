044d4370f6abe0684ddab22ab64763fb2af440c1
Agron Tsai <agrontsai@meta.com>
2026-03-19 02:16:53 -0700

    [triton][beta] [Cherry-pick] '[AMD][GLUON] Add layout in make tensor descriptor (#8355)'
    Summary:
    This is a cherry-pick of an upstream PR: https://github.com/triton-lang/triton/pull/8355
    
    Upstream commit message:
    ```
    > [AMD][GLUON] Add layout in make tensor descriptor (#8355)
    
    > #8333 uses the existing `create_make_tensor_descriptor` from triton for
    > the `tdm.make_tensor_descriptor`. It will produce a tensor descriptor
    > type without the layout. This PR exposes a new
    > `create_make_tensor_descriptor` from gluon, allows to explicitly specify
    > the return type.
    ```
    
    ***Do not remove the following line from this commit***
    Reactor Cherry-pick Revision: 73b5dc128f6227caaeb4643a3ce2b04ce027bd1e
     ---
    
    This diff was generated by running:
    ```
    buck run fbcode//triton/tools/reactor:reactor -- cherrypick --num-commits 1 --no-submit
    ```
    
    Fixed Failures:
    - gluon_ir.cc: Added missing `descPtr` argument to `MakeTensorDescOp::create` call.
      Our beta fork has an additional optional `descPtr` parameter that upstream doesn't have.
      Fix: pass `mlir::Value()` as the null descPtr value.
    
    Diff Comparison: https://www.internalfb.com/intern/paste/P2241873929/
    
    Reviewed By: stashuk-olek
    
    Differential Revision: D97097433
    
    fbshipit-source-id: 618661a5e7ca5bc9a49ad3af1de1b915fd7f88f0

--- python/src/gluon_ir.cc
+++ python/src/gluon_ir.cc
@@ -764,6 +764,14 @@ void init_gluon_ir(py::module &&m) {
              self.create<ttag::BufferLoadToLocalOp>(
                  dest, ptr, offsets, mask, other, stride, cacheModifier);
            })
+      .def("create_make_tensor_descriptor",
+           [](TritonOpBuilder &self, Type resultTy, Value &base,
+              std::vector<Value> &shape, std::vector<Value> &strides,
+              tt::PaddingOption paddingOption) -> Value {
+             return self.create<tt::MakeTensorDescOp>(
+                 resultTy, base, shape, strides,
+                 /*descPtr=*/mlir::Value(), paddingOption);
+           })
       .def("create_async_tdm_copy_global_to_local",
            [](GluonOpBuilder &self, Value descPtr, std::vector<Value> &indices,
               Value result) {
