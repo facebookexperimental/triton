b3866b885f640afc83c2a2ce54fde5c241877caf
Agron Tsai <agrontsai@meta.com>
2026-04-15 11:32:55 -0700

    [triton][beta] [Cherry-pick] '[PROTON] Introduce the `clear_data` API to remove profile data collected before deactivation (#8971)'
    Summary:
    This is a cherry-pick of an upstream PR: https://github.com/triton-lang/triton/pull/8971
    
    Upstream commit message:
    ```
    > [PROTON] Introduce the `clear_data` API to remove profile data collected before deactivation (#8971)
    ```
    
    Diff Comparison: P2268840429
    
    ***Do not remove the following line from this commit***
    Reactor Cherry-pick Revision: 037bb02ea27a2b1db8315cb8261b77a8389a388b
     ---
    
    This diff was generated by running:
    ```
    buck run fbcode//triton/tools/reactor:reactor -- cherrypick --num-commits 1 --no-submit
    ```
    
    Reviewed By: dhruvak3
    
    Differential Revision: D100434809
    
    fbshipit-source-id: e6c389a9cc2df81fc01fadb65acf9dbe307466e9

--- third_party/proton/test/test_profile.py
+++ third_party/proton/test/test_profile.py
@@ -15,7 +15,6 @@ import threading
 import triton.language as tl
 from triton.profiler.hooks.launch import COMPUTE_METADATA_SCOPE_NAME
 import triton.profiler.hooks.launch as proton_launch
-import triton.profiler.viewer as viewer
 from triton._internal_testing import is_hip
 
 
@@ -223,6 +222,7 @@ def test_get_data(tmp_path: pathlib.Path):
     proton.deactivate(session)
 
     database = proton.get_data(session)
+    import triton.profiler.viewer as viewer
     gf, _, _, _ = viewer.get_raw_metrics(database)
     foo_frame = gf.filter("MATCH ('*', c) WHERE c.'name' =~ '.*foo.*' AND c IS LEAF").dataframe
     ones_frame = gf.filter("MATCH ('*', c) WHERE c.'name' =~ '.*elementwise.*' AND c IS LEAF").dataframe
@@ -232,6 +232,33 @@ def test_get_data(tmp_path: pathlib.Path):
     assert int(foo_frame["count"].values[0]) == 2
     assert len(ones_frame) == 1
     assert int(ones_frame["count"].values[0]) == 1
+
+
+def test_clear_data(tmp_path: pathlib.Path):
+    temp_file = tmp_path / "test_clear_data.hatchet"
+    session = proton.start(str(temp_file.with_suffix("")), context="shadow")
+
+    with proton.scope("test0"):
+        x = torch.ones((2, 2), device="cuda")
+        x + x  # type: ignore
+
+    proton.deactivate(session)
+    proton.clear_data(session)
+    database = proton.get_data(session)
+    assert database[0]["children"] == []
+    assert database[0]["frame"]["name"] == "ROOT"
+
+    proton.activate(session)
+    with proton.scope("test1"):
+        x * x  # type: ignore
+    proton.deactivate(session)
+    database = proton.get_data(session)
+
+    proton.finalize()
+    assert len(database[0]["children"]) == 1
+    assert database[0]["children"][0]["frame"]["name"] == "test1"
+    kernel_frame = database[0]["children"][0]["children"][0]
+    assert "elementwise" in kernel_frame["frame"]["name"]
 
 
 def test_hook_launch(tmp_path: pathlib.Path):
