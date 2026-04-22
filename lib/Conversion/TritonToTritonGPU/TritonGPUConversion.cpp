da0e296130faa64df232e92ab59e355a29dc24d7
Shucai Xiao <shucai.xiao@amd.com>
2026-04-22 16:00:40 -0500

    [TLX] add local gather/scatter ops (#1270)
    This PR is to add tlx.local_gather/scatter operators. If follows the gluon implementation in the upstream triton PR: triton-lang/triton#8480

--- lib/Conversion/TritonToTritonGPU/TritonGPUConversion.cpp
+++ lib/Conversion/TritonToTritonGPU/TritonGPUConversion.cpp
@@ -108,6 +108,7 @@ TritonGPUConversionTarget::TritonGPUConversionTarget(
       triton::gpu::AsyncCopyGlobalToLocalOp, triton::gpu::LocalLoadOp,
       triton::gpu::LocalStoreOp, triton::gpu::RemoteShmemStoreOp,
       triton::gpu::AsyncRemoteShmemStoreOp,
+      triton::gpu::LocalGatherOp, triton::gpu::LocalScatterOp, 
       triton::nvidia_gpu::WarpGroupDotWaitOp,
       triton::nvidia_gpu::VoteBallotSyncOp, triton::tlx::RequireLayoutOp,
       triton::tlx::ReleaseLayoutOp, triton::tlx::LocalAliasOp>(
