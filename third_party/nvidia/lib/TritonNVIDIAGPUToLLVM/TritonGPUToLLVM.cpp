a90b6a19718d47a4362c3b0ede6a7de84a340101
hoy <hoy@meta.com>
2026-05-07 19:49:50 -0700

    [TLX] Guard remote and local bars init together to simplify cluster sync op insertion (#1262)
    Summary:
    What we tried to do before:
    - Identify remote bars
    - Identify their init ops via `getBackwardSliceWithWS`
    - Enforce these init ops to be at first block of func
    - Insert a cluster sync after the last init op
    
    However, making `getBackwardSliceWithWS` generic and robust is challenging and not worth it at the moment. There's at least one bug where if a block arg comes from previous blocks instead of parent WS partition Op, we'd still blindly map it to the WS Op's operand at the same index, which is wrong. In the example below, it would incorrectly map `bar_idx` to `arg0/other_bar`
    
    ```mlir
        %cta_bars = ttg.local_alloc : () -> !ttg.memdesc<2xi64, #shared, #smem, mutable>  // cta-bars-def
        %other_bar = ttg.local_alloc : () -> !ttg.memdesc<2xi64, #shared, #smem, mutable>  // other-bar-def
    
        ttg.warp_specialize(%other_bar, %cta_bars) attributes {warpGroupStartIds = array<i32: 4>}
        default {
          ttg.warp_yield
        }
        partition0(%arg0: !ttg.memdesc<2xi64, #shared, #smem, mutable>, %arg1: !ttg.memdesc<2xi64, #shared, #smem, mutable>) num_warps(1) {
          %c0 = arith.constant 0 : i32
          ...
          cf.br ^bb1(%c0 : i32)
    
        ^bb1(%bar_idx: i32):
          %cmp = arith.cmpi slt, %bar_idx, %c10 : i32
          cf.cond_br %cmp, ^bb2, ^bb3
    
        ^bb2:
          %idx = arith.remsi %bar_idx, %c2 : i32
          %bar = ttg.memdesc_index %arg1[%idx] : !ttg.memdesc<2xi64, #shared, #smem, mutable> -> !ttg.memdesc<1xi64, #shared, #smem, mutable>  // bar-def
          %remote = ttng.map_to_remote_buffer %bar, %c0 {slice_target} : !ttg.memdesc<1xi64, #shared, #smem, mutable> -> !ttg.memdesc<1xi64, #shared, #ttng.shared_cluster_memory, mutable>  // remote-def
    
    ```
    
    To minimize the chance of inserting cluster sync too early, we do not differentiate remote or local bars when find the last bar init. So the flow looks like:
    
    - Identify remote bars
    - Identify init ops of *all bars*
    - Enforce these init ops to be at first block of func
    - Insert a cluster sync after the last init op
    
    This means we could possibly delay the cluster sync very slightly to after some local bar init ops in exchange for more safety. There should be very minimal disruption to existing kernels (unless they have local bar init not located in the first block), and we believe the perf cost should be very minimal as well. Plus TLX users can always move the bar alloc API calls to earlier in the kernel to recover some perf.
    
    Pull Request resolved: https://github.com/facebookexperimental/triton/pull/1262
    
    Test Plan: lit test
    
    Reviewed By: htyu
    
    Differential Revision: D101080833
    
    Pulled By: pchen7e2
    
    fbshipit-source-id: 771d9d82fe397dfcc7923df74114714d2cf33dde

--- third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TritonGPUToLLVM.cpp
+++ third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TritonGPUToLLVM.cpp
@@ -232,56 +232,8 @@ private:
         static_cast<unsigned>(NVVM::NVVMMemorySpace::Shared));
   }
 
-  void getBackwardSliceWithWS(Value target,
-                              SetVector<Operation *> *backwardSlice) {
-    SetVector<Value> worklist;
-    worklist.insert(target);
-
-    BackwardSliceOptions options;
-    options.omitUsesFromAbove = false;
-    options.omitBlockArguments = true;
-    options.inclusive = true;
-
-    while (!worklist.empty()) {
-      Value nextTarget = worklist.back();
-      worklist.pop_back();
-
-      if (auto arg = dyn_cast<BlockArgument>(nextTarget)) {
-        if (auto wsPartitionOp = dyn_cast<ttg::WarpSpecializePartitionsOp>(
-                arg.getOwner()->getParentOp())) {
-          auto argIndex = arg.getArgNumber();
-          auto wsOp = wsPartitionOp->getParentOfType<ttg::WarpSpecializeOp>();
-          // map to WSOp's operand at the same index
-          nextTarget = wsOp.getOperand(argIndex);
-        } else {
-          // ttg::WarpSpecializeOp's default region just captures
-          // from trunk so no need to special handle the defining block args.
-          // We should omit block args for other block structures like scf.For,
-          // and the captures would still be handled automatically
-          continue;
-        }
-      }
-
-      SetVector<Operation *> ops;
-      if (failed(getBackwardSlice(nextTarget, &ops, options))) {
-        llvm_unreachable("getBackwardSlice failed");
-      }
-
-      for (auto op : ops) {
-        if (backwardSlice->insert(op)) {
-          for (auto operand : op->getOperands()) {
-            if (isa<BlockArgument>(operand)) {
-              worklist.insert(operand);
-            }
-          }
-        }
-      }
-    }
-  }
-
-  LogicalResult
-  ensureEarlyRemoteBarInit(ModuleOp &mod,
-                           SetVector<Operation *> &remoteBarInitOps) {
+  LogicalResult ensureEarlyBarInit(ModuleOp &mod,
+                                   SetVector<Operation *> &barInitOps) {
     triton::FuncOp funcOp = nullptr;
     mod.walk([&](triton::FuncOp op) {
       if (triton::isKernel(op)) {
@@ -291,10 +243,10 @@ private:
       return WalkResult::advance();
     });
     assert(funcOp && "Expecting to find a kernel func but got none.");
-    for (auto op : remoteBarInitOps) {
+    for (auto op : barInitOps) {
       if (op->getBlock() != &funcOp.front()) {
         op->emitError() << "Barrier init outside of the first block in "
-                           "function is not supported for remote barriers";
+                           "function is not supported";
         return failure();
       }
     }
@@ -395,7 +347,7 @@ private:
     // All remote barrier init ops need to happen at the first block of
     // function. This is to make 2cta cluster sync insertion easier for WarpSpec
     // case.
-    if (failed(ensureEarlyRemoteBarInit(mod, remoteOrLocalBarInitOps))) {
+    if (failed(ensureEarlyBarInit(mod, remoteOrLocalBarInitOps))) {
       return failure();
     }
 
