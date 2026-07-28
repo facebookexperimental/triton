df95ff2d09616f5210508db1326f2f49e32d9355
peterbell10 <peterbell10@openai.com>
2026-07-28 01:15:40 +0100

    [Reapply][Backend] Fix WarpGroupDotWaitOp semantics when num_warps > 4 (#11056)
    Reapplies #9484, Fixes #11047
    
    This reapplies the main bugfix, and also avoids a performance regression
    by having MemBar automatically mark warp group dot wait ops as
    warp-group local if there is a subsequent barrier-like op before any
    other memory effects.
    
    (cherry picked from commit 05bf2b3ff265c18ed8b2fe618af7d5c71af9018f)

--- lib/Analysis/Membar.cpp
+++ lib/Analysis/Membar.cpp
@@ -294,6 +294,8 @@ bool containsLocalBarrier(Operation *op) {
     return true;
   if (auto barrier = dyn_cast<triton::gpu::BarrierOp>(op))
     return barrier.hasLocal();
+  if (auto wgWait = dyn_cast<triton::nvidia_gpu::WarpGroupDotWaitOp>(op))
+    return !wgWait.getWarpGroupLocal() && triton::gpu::lookupNumWarps(op) > 4;
   return false;
 }
 
@@ -400,6 +402,16 @@ static bool hasSyncPointBeforeMemoryEffect(Operation *op,
 void MembarAnalysis::update(Operation *op, BlockInfo *blockInfo,
                             FuncBlockInfoMapT *funcBlockInfoMap,
                             OpBuilder *builder) {
+  // A later CTA-wide synchronization can also synchronize this wait, provided
+  // no memory is accessed before reaching it.
+  if (auto wgWait = dyn_cast<triton::nvidia_gpu::WarpGroupDotWaitOp>(op)) {
+    if (!wgWait.getWarpGroupLocal() &&
+        triton::gpu::lookupNumWarps(wgWait) > 4 &&
+        hasSyncPointBeforeMemoryEffect(wgWait, allocation)) {
+      wgWait->setAttr("warpGroupLocal", builder->getUnitAttr());
+    }
+  }
+
   auto arrive = dyn_cast<triton::nvidia_gpu::ArriveBarrierOp>(op);
   auto barrierStages = getLocalBarrierStages(op, allocation);
   if (barrierStages.beforeMemoryEffects) {
