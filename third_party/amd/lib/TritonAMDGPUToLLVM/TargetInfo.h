2bc49b5ef92400efe552afd48c44e779f15b070f
Frederick Vu <100011202+FrederickVu@users.noreply.github.com>
2026-08-01 17:33:00 -0700

    [AMD][NFC][Backend] Refactor ds_read_tr lowerings (#11144)
    (cherry picked from commit c3aa0c5ff0d38c789c5614033e631e0744616252)

--- third_party/amd/lib/TritonAMDGPUToLLVM/TargetInfo.h
+++ third_party/amd/lib/TritonAMDGPUToLLVM/TargetInfo.h
@@ -61,7 +61,6 @@ public:
                                            Value coordinate) const override;
 
   // Describes the parameters of ds_read_tr for a particular data type.
-  using TileKind = amdgpu::TargetFeatures::TileKind;
   using LDSTransLoadParams = amdgpu::TargetFeatures::LDSTransLoadParams;
   // Get the ds_read_tr parameters for the instruction that operates on the
   // element granularity specified by bitWidth. Returns candidates ordered from
