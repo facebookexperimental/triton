0582e3191850ac2fe9dca9b5c27227f14eb2358e
Nick Riasanovsky <njriasan@meta.com>
2026-08-03 14:58:40 -0700

    [triton][beta] [Cherry-pick] '[NVIDIA] Support multi-CTA local gather and scatter (#10472)' (#2622)
    Summary:
    Pull Request resolved: https://github.com/facebookexperimental/triton/pull/2622
    
    This is a backport of an upstream PR: https://github.com/triton-lang/triton/pull/10472
    
    Upstream commit message:
    ```
    > [NVIDIA] Support multi-CTA local gather and scatter (#10472)
    
    > PR description written by Codex
    
    > ## Summary
    > - Add multi-CTA support to `ttg.local_gather` and `ttg.local_scatter`.
    
    > ## Stack
    > Merge bottom-up:
    > - [ ] 👉 #10472
    > - [ ] #10473
    > - [ ] #10527
    > - [ ] #10532
    > - [ ] #10533
    > - [ ] #10542
    > - [ ] #10548
    ```
    
    ***Do not remove the following line from this commit***
    Reactor Backport Revision: bb67bbd54ab9a011e10bfd66ad67dd74900153d6
     ---
    
    This diff was generated by running:
    ```
    buck run fbcode//triton/tools/reactor:reactor -- backport --pr 10472
    ```
    
    Differential Revision: D114445787

--- third_party/amd/lib/TritonAMDGPUToLLVM/TargetInfo.h
+++ third_party/amd/lib/TritonAMDGPUToLLVM/TargetInfo.h
@@ -47,10 +47,10 @@ public:
   void warpSync(Location loc, RewriterBase &rewriter) const override;
 
   void storeDShared(RewriterBase &rewriter, Location loc, Value ptr,
-                    std::optional<Value> ctaId, Value val, Value pred,
+                    Value ctaId, Value val, Value pred,
                     std::optional<Value> barrierPtr = {}) const override;
   Value loadDShared(RewriterBase &rewriter, Location loc, Value ptr,
-                    std::optional<Value> ctaId, Type elemTy, Value pred,
+                    Value ctaId, Type elemTy, Value pred,
                     Operation *localLoadOp = nullptr) const override;
 
   // Describes the parameters of ds_read_tr for a particular data type.
