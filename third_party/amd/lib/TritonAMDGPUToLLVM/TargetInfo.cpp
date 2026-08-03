a3774a2212c6f26aca6d7806151f4596085cc308
Nick Riasanovsky <njriasan@meta.com>
2026-08-03 14:16:34 -0700

    [triton][beta] [Cherry-pick] '[NVIDIA] Support multi-CTA local gather and scatter (#10472)'
    Summary:
    This is a backport of an upstream PR: https://github.com/triton-lang/triton/pull/10472
    
    Upstream commit message:
    ```
    > [NVIDIA] Support multi-CTA local gather and scatter (#10472)
    
    > PR description written by Codex
    
    > ## Summary
    > - Add multi-CTA support to `ttg.local_gather` and `ttg.local_scatter`.
    
    > ## Stack
    > Merge bottom-up:
    > - [ ] 👉 #10472
    > - [ ] #10473
    > - [ ] #10527
    > - [ ] #10532
    > - [ ] #10533
    > - [ ] #10542
    > - [ ] #10548
    ```
    
    ***Do not remove the following line from this commit***
    Reactor Backport Revision: bb67bbd54ab9a011e10bfd66ad67dd74900153d6
     ---
    
    This diff was generated by running:
    ```
    buck run fbcode//triton/tools/reactor:reactor -- backport --pr 10472
    ```
    
    Differential Revision: D114445787

--- third_party/amd/lib/TritonAMDGPUToLLVM/TargetInfo.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/TargetInfo.cpp
@@ -183,9 +183,9 @@ void TargetInfo::warpSync(Location loc, RewriterBase &rewriter) const {
 }
 
 void TargetInfo::storeDShared(RewriterBase &rewriter, Location loc, Value ptr,
-                              std::optional<Value> ctaId, Value val, Value pred,
+                              Value ctaId, Value val, Value pred,
                               std::optional<Value> barrierPtr) const {
-  if (ctaId.has_value()) {
+  if (ctaId) {
     llvm::report_fatal_error(
         "AMDGPU does not support cross-CTA shared memory transfers");
   }
@@ -205,9 +205,9 @@ TargetInfo::queryLDSTransLoadParams(int bitWidth) const {
 }
 
 Value TargetInfo::loadDShared(RewriterBase &rewriter, Location loc, Value ptr,
-                              std::optional<Value> ctaId, Type elemTy,
-                              Value pred, Operation *localLoadOp) const {
-  if (ctaId.has_value()) {
+                              Value ctaId, Type elemTy, Value pred,
+                              Operation *localLoadOp) const {
+  if (ctaId) {
     llvm::report_fatal_error(
         "AMDGPU does not support cross-CTA shared memory transfers");
   }
