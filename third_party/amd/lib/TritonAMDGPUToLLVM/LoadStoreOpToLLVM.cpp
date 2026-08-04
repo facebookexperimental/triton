cc178823382deda4b2c00c5eb73b2700fa90793e
Nick Riasanovsky <njriasan@meta.com>
2026-08-04 12:44:36 -0700

    [triton][beta] [Cherry-pick] '[AMD][gfx1250] Multi-CTA/Multicast support for TDM Gather (#10674)' (#2669)
    Summary:
    Pull Request resolved: https://github.com/facebookexperimental/triton/pull/2669
    
    This is a backport of an upstream PR: https://github.com/triton-lang/triton/pull/10674
    
    Upstream commit message:
    ```
    > [AMD][gfx1250] Multi-CTA/Multicast support for TDM Gather (#10674)
    
    > Adds multi-cta support for TDM gather. Since the indices only describes
    > rows their CGA layout does not have to strictly match the CGA layout of
    > the shared encoding, only the row dimension has to match. How columns
    > are distributed across CTAs is only described by the CGALayout of the
    > shared encoding.
    ```
    
    ***Do not remove the following line from this commit***
    Reactor Backport Revision: af227557e15682dfbd08e256102f77bb98413092
     ---
    
    This diff was generated by running:
    ```
    buck run fbcode//triton/tools/reactor:reactor -- backport --pr 10674
    ```
    
    Reviewed By: warrendeng
    
    Differential Revision: D114493748
    
    fbshipit-source-id: 8cde237a17234e964a44b697129b22ae4db03a72

--- third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
@@ -1523,8 +1523,8 @@ struct AsyncTDMScatterOpConversion
     Value pred = arith::ConstantIntOp::create(rewriter, loc, 1, 32);
     mlir::LLVM::AMD::emitTDMGatherScatter(
         rewriter, loc, getTypeConverter(), desc, shapePerCTA, padInterval,
-        padAmount, srcPtr, pred, elementType, barrierPtr, cgaLayout, ctaId,
-        dstRowIndices, dstColOffset,
+        padAmount, srcPtr, pred, /*multicastMask=*/{}, elementType, barrierPtr,
+        cgaLayout, ctaId, dstRowIndices, dstColOffset,
         /*isGather=*/false, numWarps, dstRowIndicesType);
 
     rewriter.eraseOp(op);
@@ -1549,11 +1549,6 @@ struct AsyncTDMGatherOpConversion
                   ConversionPatternRewriter &rewriter) const override {
     auto loc = op.getLoc();
     auto b = TritonLLVMOpBuilder(loc, rewriter);
-
-    // Multi-CTA not supported for gather
-    if (lookupNumCTAs(op) > 1) {
-      return op.emitError("TDM gather does not support multi-CTA");
-    }
 
     auto tensorDescTy = op.getDesc().getType();
     auto smemTy = op.getDst().getType();
@@ -1616,10 +1611,18 @@ struct AsyncTDMGatherOpConversion
         {kBlock}, to_vector(sharedLayout.getOutDimNames()));
     auto ctaId = targetInfo.getClusterCTAId(rewriter, loc);
 
+    Value multicastMask;
+    if (targetInfo.supportsMultiCTALaunch()) {
+      // Use the sharedLayout to compute the multicast mask because the index
+      // layout only describes rows and misses information about columns.
+      multicastMask =
+          LLVM::AMD::emitCtaMulticastMask(rewriter, loc, ctaId, sharedLayout);
+    }
+
     mlir::LLVM::AMD::emitTDMGatherScatter(
         rewriter, loc, getTypeConverter(), desc, shapePerCTA, padInterval,
-        padAmount, dstPtr, op.getPred(), elementType, barrierPtr, cgaLayout,
-        ctaId, srcRowIndices, srcColOffset,
+        padAmount, dstPtr, op.getPred(), multicastMask, elementType, barrierPtr,
+        cgaLayout, ctaId, srcRowIndices, srcColOffset,
         /*isGather=*/true, numWarps, srcRowIndicesType);
 
     rewriter.eraseOp(op);
