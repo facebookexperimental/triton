0ba2f3f4efdcee2705f5560c97acd6dd68fa5b65
mren <mren@meta.com>
2026-08-28 15:31:33 -0700

    fix build
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- setup.py
+++ setup.py
@@ -419,7 +419,8 @@ class CMakeBuild(build_ext):
         subprocess.check_call(["cmake", "--build", ".", "--target", "mlir-doc"], cwd=cmake_dir)
 
 
-backends = [*BackendInstaller.copy(["nvidia", "amd", "cpu"]), *BackendInstaller.copy_externals()]
+_codegen_backends = os.getenv("TRITON_CODEGEN_BACKENDS", "nvidia;amd;cpu").split(";")
+backends = [*BackendInstaller.copy(_codegen_backends), *BackendInstaller.copy_externals()]
 
 
 def get_package_dirs():
