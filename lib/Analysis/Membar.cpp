955763994341f3a1290f60c051a05c66bfd2a55b
Agron Tsai <agrontsai@fb.com>
2026-09-22 17:30:33 -0700

    [sync-train] Repair local gate (plan-65df5edb3320fabb item 1)

--- lib/Analysis/Membar.cpp
+++ lib/Analysis/Membar.cpp
@@ -294,7 +294,7 @@ bool containsLocalBarrier(Operation *op) {
     return true;
   if (auto barrier = dyn_cast<triton::gpu::BarrierOp>(op))
     return barrier.hasLocal();
-  if (auto wgWait = dyn_cast<ttng::WarpGroupDotWaitOp>(op))
+  if (auto wgWait = dyn_cast<triton::nvidia_gpu::WarpGroupDotWaitOp>(op))
     return !wgWait.getWarpGroupLocal() && triton::gpu::lookupNumWarps(op) > 4;
   return false;
 }
@@ -406,7 +406,7 @@ void MembarAnalysis::update(Operation *op, BlockInfo *blockInfo,
 
   // A later CTA-wide synchronization can also synchronize this wait, provided
   // no memory is accessed before reaching it.
-  if (auto wgWait = dyn_cast<ttng::WarpGroupDotWaitOp>(op)) {
+  if (auto wgWait = dyn_cast<triton::nvidia_gpu::WarpGroupDotWaitOp>(op)) {
     if (!wgWait.getWarpGroupLocal() &&
         triton::gpu::lookupNumWarps(wgWait) > 4 &&
         hasSyncPointBeforeMemoryEffect(wgWait, allocation)) {
