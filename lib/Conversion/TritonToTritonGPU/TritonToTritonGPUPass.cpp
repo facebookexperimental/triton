caf1962231de557e657243e381a20487ecb6422e
Agron Tsai <agrontsai@fb.com>
2026-09-23 04:52:11 -0700

    [sync-train] Repair OSS CI (plan-b946f3a98edeeb5d item 1)

--- lib/Conversion/TritonToTritonGPU/TritonToTritonGPUPass.cpp
+++ lib/Conversion/TritonToTritonGPU/TritonToTritonGPUPass.cpp
@@ -675,8 +675,13 @@ void populateTritonPatterns(TritonGPUTypeConverter &typeConverter,
 // tensors in for Triton object specific tracing operations in which case we
 // would need to fill in the OpConversionPattern
 void populateTLXPatterns(TritonGPUTypeConverter &typeConverter,
-                            RewritePatternSet &patterns) {
+                         RewritePatternSet &patterns,
+                         TritonGPUConversionTarget &target) {
   MLIRContext *context = patterns.getContext();
+  target.addDynamicallyLegalOp<triton::amdgpu::AssumeUniformOp>(
+      [&typeConverter](triton::amdgpu::AssumeUniformOp op) {
+        return typeConverter.isLegal(op.getOperation());
+      });
   patterns.add<GenericOpPattern<triton::gpu::WarpVoteOp>>(typeConverter,
                                                           context);
   patterns.add<GenericOpPattern<triton::tlx::RequireLayoutOp>>(typeConverter, context);
@@ -948,7 +953,7 @@ public:
     FuncArgRenamer renamer;
     populateFunctionTypeConversions(typeConverter, renamer, patterns);
     populateTritonPatterns(typeConverter, patterns, numCTAs);
-    populateTLXPatterns(typeConverter, patterns);
+    populateTLXPatterns(typeConverter, patterns, target);
     // TODO: can we use
     //    mlir::scf::populateSCFStructurealTypeConversionsAndLegality(...) here?
     populateSCFPatterns(typeConverter, patterns);
